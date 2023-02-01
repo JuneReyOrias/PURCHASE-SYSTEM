4896794adc1ac6bf1f963e352698990c4423318b
juneOrias <117705030+JuneReyOrias@users.noreply.github.com>
2023-02-01 08:07:17 +0800

    Add files via upload

---
+++ Upress-projectEdit/database/database.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$dbUsername = "root";
+$dbpassword="";
+$dbName = "dbUpresssysytem";
+
+$conn =mysqli_connect($servername, $dbUsername, $dbpassword, $Name);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ Upress-projectEdit/includes/sidebar.php
@@ -0,0 +1,36 @@
+
+<div class="side-bar">
+    <div class="logo-details">
+        <img src="image" class="img-fluid">
+        <span class="logo-name">UPRESS</span>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="../customer/dashboard.php" class="<?php echo $dashboard; ?>">
+                <i class='bx bx-grid-alt' ></i>
+                <span class="links-name">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="<?php echo $purchase; ?>">
+                <i class='bx bx-send'></i>
+                <span class="links-name">Purchase</span>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="<?php echo $order; ?>">
+                <i class='bx bx-phone-call'></i>
+                <span class="links-name">Order</span>
+            </a>
+        </li>
+            </a>
+        </li>
+        <hr class="line">
+        <li id="logout-link">
+            <a href="../login/logout.php">
+                <i class='bx bx-log-out'></i>
+                <span class="links-name">Logout</span>
+            </a>
+        </li>
+    </ul>
+</div>
