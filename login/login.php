19978f7b8bd2d2a3838a00951a05cd3c8ba88a2f
JoelMadd <91931560+JoelMadd@users.noreply.github.com>
2023-02-15 16:22:16 +0800

    Update login.php

--- login/login.php
+++ login/login.php
@@ -60,9 +60,9 @@ $accounts = array(
             </div>
             <hr class="divider">
             <label for="username">Username</label>
-            <input type="text" id="username" name="username" placeholder="username" required tabindex="1">
+            <input type="text" id="username" name="username" placeholder="Enter username" required tabindex="1">
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="password" required tabindex="2">
+            <input type="password" id="password" name="password" placeholder="Enter password" required tabindex="2">
             <input class="button" type="submit" value="Login" name="login" tabindex="3">
             <?php
                 //Display the error message if there is any.
