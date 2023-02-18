fab50213261b91ee40f0e03102807e5959b11092
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-18 22:48:07 +0800

    Update users.class.php

--- database/users.class.php
+++ database/users.class.php
@@ -7,6 +7,7 @@ Class users{
     public $type;
     public $username;
     public $password;
+    public $email;
 
     protected $db;
 
@@ -14,6 +15,21 @@ Class users{
     {
         $this->db = new database();
     }
+ 
+    function login(){
+        $sql = "SELECT * FROM users WHERE email= :email and password = :password" ;
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+           $data = $query->fetch();
+        }
+     	return $data;
+    }
+
+
+
+
 
     function validate(){
         $sql = "SELECT * FROM users WHERE username =:username and password = :password ;";
