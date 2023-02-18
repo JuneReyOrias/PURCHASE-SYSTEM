82799cc28c41d53ec305cbc8d2fefe1d882c00a4
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-18 22:58:04 +0800

    Create user.class.php

---
+++ database/user.class.php
@@ -0,0 +1,46 @@
+<?php 
+require_once 'database.php';
+
+Class user{
+    public $firstname;
+    public $lastname;
+    public $type;
+    public $username;
+    public $password;
+    public $email;
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new database();
+    }
+ 
+    function login(){
+        $sql = "SELECT * FROM users WHERE email= :email and password = :password" ;
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+           $data = $query->fetch();
+        }
+     	return $data;
+    }
+
+
+
+
+
+    function validate(){
+        $sql = "SELECT * FROM users WHERE username =:username and password = :password ;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':username', $this->username);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+            $data = $query->fetch();
+        }
+        return $data;
+    }
+}
+
+?>
