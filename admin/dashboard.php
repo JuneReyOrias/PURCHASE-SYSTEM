2911942b787236533bdf1078ccdadca312855dde
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-20 17:37:58 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -65,7 +65,7 @@
 
                     <li class="nav-link">
                         <a href="purchase.php">
-                            <i class='bx bx-Service icon' ></i>
+                            <i class='bx bx-service icon' ></i>
                             <span class="text nav-text">Services</span>
                         </a>
                     </li>
