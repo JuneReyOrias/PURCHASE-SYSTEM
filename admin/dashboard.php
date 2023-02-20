cc66a5149670ae2f616350781370118ed1e39f15
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-02-20 18:05:18 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -65,7 +65,7 @@
 
                     <li class="nav-link">
                         <a href="purchase.php">
-                            <i class='bx bx-cog icon' ></i>
+                            <i class='fa fa-service icon' ></i>
                             <span class="text nav-text">Services</span>
                         </a>
                     </li>
