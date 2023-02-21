852a6f08b9ed3d398d52daf1ca482065de23b503
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-22 00:07:19 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -138,14 +138,14 @@
         <div class="home-content">
     <div class="checkbox-container">              
             <form class="checkbox-form" action="print.php" method="post">
-            <select class="select" multiple>
-  <option value="1">One</option>
+              
+            <select class="select">
+  <option placehoder="design"></option>
   <option value="2">Two</option>
   <option value="3">Three</option>
   <option value="4">Four</option>
-  <option value="5">Five</option>
+
 </select>
-<label class="form-label select-label">Design Required?</label>
 
                     </div>
                 </div>
