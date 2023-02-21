e351a0e52bd5248daa9f008853fb12623d8b73f8
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-21 12:46:02 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -154,18 +154,17 @@
                             <i class='bx bx-box' ></i>
                             <span class="text nav-text">box</span>
                         
-                    </li>
-
-                    <li class="nav-link">
-                      
-                      <i class='bx bx-box' ></i>
-                      <span class="text nav-text">box</span>
                   
-              </li>
-              <form action="checkbox-form.php" method="post">
-    Do you need wheelchair access?
-    <input type="checkbox" name="formWheelchair" value="Yes" />
-    <input type="submit" name="formSubmit" value="Submit" />
+
+Which buildings do you want access to?<br />
+<input type="checkbox" name="formDoor[]" value="A" />Acorn Building<br />
+<input type="checkbox" name="formDoor[]" value="B" />Brown Hall<br />
+<input type="checkbox" name="formDoor[]" value="C" />Carnegie Complex<br />
+<input type="checkbox" name="formDoor[]" value="D" />Drake Commons<br />
+<input type="checkbox" name="formDoor[]" value="E" />Elliot House
+
+<input type="submit" name="formSubmit" value="Submit" />
+
 </form>
                     
                     </div>
