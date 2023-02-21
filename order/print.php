e76cdc4631f617a36c2c32550e95fe969519d288
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-22 03:38:29 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -138,7 +138,7 @@
         <div class="home-content">
     <div class="select-container">              
             <form class="select-form" action="order.php" method="post">
-   <div><
+   <div>
         <label class="design"for=""><i class='bx bx-printer icon' ></i>Printing type</label>
         <select name="Printing type "class="select multiple-select" multiple >
         
@@ -148,11 +148,10 @@
         <option value="">Rhisograph</option>
         <option value="">Kromekote</option>
 </div>
-           <div>
-           <label class="qty"for="">Digital</label>
-   
-              
-           </div>   
+<div>
+        <label class="design"for=""><i class='bx bx-printer icon' ></i>Printing type</label>
+
+ 
        
 </form> 
                     </div>
