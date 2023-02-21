35836924ca0da090a71f43cb7f0e9e47d15f6927
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-21 12:19:27 +0800

    Update user.class.php

--- database/user.class.php
+++ database/user.class.php
@@ -19,7 +19,7 @@ Class users{
     }
  
     function login(){
-        $sql = "SELECT * FROM user_acc WHERE username = :email and user_pass = :password" ;
+        $sql = "SELECT * FROM user_acc WHERE user_name = :email and user_pass = :password" ;
         $query=$this->db->connect()->prepare($sql);
         $query->bindParam(':email', $this->email);
         $query->bindParam(':password', $this->password);
