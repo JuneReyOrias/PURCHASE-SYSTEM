fc6b0a85d59b9789a4cd921a98ae60394360b36f
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-22 01:00:31 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -141,9 +141,9 @@
         <label class="design"for="design required?">Design Required?</label>
         <select class="select">
         <option value="2"></option>
-        <option value="2">No, I haconst variableName = ;</option>
-        <option value="3">Three</option>
-        <option value="4">Four</option>
+        <option value="">No, I have a print ready file = ;</option>
+        <option value="3">Yes, I will require layouting</option>
+        <option value="4">My file needs some changes</option>
               
 </form> 
                     </div>
