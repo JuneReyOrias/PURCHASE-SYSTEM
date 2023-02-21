be77d965b9fc7cb0dec04b0bd606b670c3775461
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-21 15:45:56 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -138,8 +138,9 @@
         <div class="home-content">
     <div class="checkbox-container">              
             <form class="checkbox-form" action="order.php" method="post">
+            <h3>Printing</h3>
              <input class ="check" type="checkbox" name="Printing" value="Printing">
-             <span class="text-box">Printing</span>  </input>
+         
             <input type="checkbox" name="Binding" value="Binding" >
             <span class="text-box">Printing</span>  </input>
             <input type="checkbox" name="Laminate" value="Laminate">
