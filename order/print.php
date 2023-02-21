ee034a5c7ade5df1e157f74733f03f75d9a7ecb1
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-22 03:53:37 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -170,10 +170,10 @@
 </nav>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
-    $(".multiple-select").select2({
+   // $(".multiple-select").select2({
    // maximumSelectionLength: 1
     
-});
+//});
 </script>
 <script>
         const body = document.querySelector('body'),
