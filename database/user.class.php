d3d09cec126357101f1ea4d51414948bc07c3a63
JoelMadd <91931560+JoelMadd@users.noreply.github.com>
2023-02-21 10:08:56 +0800

    rm gitignore

---
+++ database/user.class.php
@@ -0,0 +1,76 @@
+<?php 
+require_once '../database/database.php';
+
+Class users{
+    public $username;
+    public $password;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $role;
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new database();
+    }
+ 
+    function login(){
+        $sql = "SELECT * FROM user_acc WHERE user_name = :email and user_pass = :password" ;
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+           $data = $query->fetch();
+        }
+     	return $data;
+    }
+
+
+/*    function login(){
+        $sql = "SELECT * FROM users WHERE BINARY email = :email AND BINARY password = :password AND type = 'customers';";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+            if($query->rowCount()>0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function sign_in_admin(){
+        $sql = "SELECT * FROM user WHERE BINARY email = :email AND BINARY password = :password AND type != 'customer';";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+            if($query->rowCount()>0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+   public function get_user_info($id=0){
+        if($id == 0){
+            $sql = "SELECT * FROM user WHERE BINARY email = :email AND BINARY password = :password;";
+            $query=$this->db->connect()->prepare($sql);
+            $query->bindParam(':email', $this->email);
+            $query->bindParam(':password', $this->password);
+        }else{
+            $sql = "SELECT * FROM users WHERE id = :id;";
+            $query=$this->db->connect()->prepare($sql);
+            $query->bindParam(':id', $id);
+        }
+        if($query->execute()){
+            $data = $query->fetchAll();
+        }
+        return $data;
+    }
+*/
+}
+
+?>
---
+++ u151063784_db_upress.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 19, 2023 at 12:57 AM
+-- Server version: 10.6.11-MariaDB-cll-lve
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u151063784_db_upress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_acc`
+--
+
+CREATE TABLE `user_acc` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_pass` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_acc`
+--
+
+INSERT INTO `user_acc` (`id`, `user_name`, `user_pass`, `firstname`, `lastname`, `email`, `role`) VALUES
+(1, 'test', 'test', 'test', 'test', 'test', 'test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_acc`
+--
+ALTER TABLE `user_acc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_acc`
+--
+ALTER TABLE `user_acc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
