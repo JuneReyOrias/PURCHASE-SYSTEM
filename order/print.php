14d5fbcb5b4b06b72d343f3ec155a2d58bd53acf
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-23 11:38:53 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -167,7 +167,6 @@
         <option value="">Rhisograph</option>
         <option value="">Kromekote</option>
 
-        
           <input class="fc" type="text" name="digitall" placeholder="Qty per type">
          
         <div class="file-folder">
@@ -175,7 +174,9 @@
         <input class="form-control" id="formFileSm" type="file" /> <br>
         
        </div>
-       <label class="design"for=""></i>Printing type</label>
+       
+        </div>
+        <label class="design"for=""></i>Printing type</label>
         <select name="Printing type "class="select multiple-select" multiple >
         
         <option value="">Digital Color</option>
@@ -184,7 +185,6 @@
         <option value="">Rhisograph</option>
         <option value="">Kromekote</option>
 
-        
           <input class="fc" type="text" name="digitall" placeholder="Qty per type">
          
         <div class="file-folder">
@@ -192,6 +192,7 @@
         <input class="form-control" id="formFileSm" type="file" /> <br>
         
        </div>
+       
         </div>
         <input class="buttons" href="order.php" type="submit" value="continue" name="continue" tabindex="3" >
 
