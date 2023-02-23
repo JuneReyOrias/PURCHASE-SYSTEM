be4e6c1d6fe924e3445cceb0418b58a755a9e400
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-23 11:58:12 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -157,8 +157,26 @@
     
         
         </div>
-        <input class="buttons" href="order.php" type="submit" value="continue" name="continue" tabindex="3" >
 
+        <div class="col2" ><spani class='bx bx-printer icon' > Printing</span></div>
+        <label class="design"for=""></i>Printing type</label>
+        <select name="Printing type "class="select multiple-select" multiple >
+        
+        <option value="">Digital Color</option>
+        <option value="">Digital Black and White</option>
+        <option value="">Photo Printing</option>
+        <option value="">Rhisograph</option>
+        <option value="">Kromekote</option>
+
+        <div>
+        <input class="fc" type="text" name="digitall" placeholder="Qty per type">
+        <input class="form-control" id="formFileSm" type="file" /> <br>
+        </div>
+        <input class="buttons" href="order.php" type="submit" value="continue" name="continue" tabindex="3" >
+        </div>
+    
+        
+    </div>
 
 </form> 
                     </div>
