7cfe895a2a2052482c5cc5399c9a7e70c415a7b6
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-23 11:12:24 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -156,9 +156,9 @@
           <input class="fc" type="text" name="digitall" placeholder="Qty per type"><br>
          
         <div class="file-folder">
-        <label for="formFileSm" class="form-label">Small file input example</label>
+       
         <input class="form-control form-control-sm" id="formFileSm" type="file" />
-        <label for="formFileLg" class="form-label">Large file input example</label>
+        
         <input class="form-control form-control-lg" id="formFileLg" type="file" />
         <input class="buttons" href="order.php" type="submit" value="continue" name="continue" tabindex="3" >
 
