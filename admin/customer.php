0d015488a1cb11721d817ceadbe714e97e4d3932
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-25 07:39:28 +0800

    Update customer.php

--- admin/customer.php
+++ admin/customer.php
@@ -214,14 +214,12 @@
         $('#add').click(function(){
             count = count + 1;
             var html_code = "<tr id='row"+count+"'>";
-            
-            var html_code = "<tr id='row"+count+"'>";
-            html_code += " <td contenteditable="true" class="user_name"></td>";
-            html_code += " <td contenteditable="true" class="password"></td>";
-            html_code +=" <td contenteditable="true" class="firstname"></td>";
-            html_code += " <td contenteditable="true" class="lastname"></td>";
-            html_code +=" <td contenteditable="true" class="email"></td>";
-            html_code += "  <td contenteditable="true" class="role"></td>";
+            html_code += "<td contenteditable='true' class='user_name'></td>";
+            html_code += "<td contenteditable='true' class='pasword'></td>";
+            html_code += "<td contenteditable='true' class='firstname'></td>";
+            html_code += "<td contenteditable='true' class='lastname text-start'></td>";
+            html_code += "<td contenteditable='true' class='email'></td>";
+            html_code += "<td contenteditable='true' class='role'></td>";
             html_code += "<td><button type='button' name='remove' data-row='row"+count+"' class='btn btn-danger btn-xs remove'>-</button></td>";   
             html_code += "</tr>";  
             $('#crud_table').append(html_code);
