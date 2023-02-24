684c5a4d52ab0c4ef07ea0663c2ae18026737d22
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-24 11:13:54 +0800

    Update form.php

--- order/form.php
+++ order/form.php
@@ -159,14 +159,7 @@
         <input class="fc" type="text" name="digitall" placeholder="Qty per type">
         <input class="form-control" id="formFileSm" type="file" />
         <input class="fc" type="text" name="digitall" placeholder="Size"><br>
-        <label class="design"for=""></i>Printing type</label>
-        <select name="Printing type "class="select multiple-select" multiple >
-        
-        <option value="">Digital Color</option>
-        <option value="">Digital Black and White</option>
-        <option value="">Photo Printing</option>
-        <option value="">Rhisograph</option>
-        <option value="">Kromekote</option>
+       
 
         <div>
         <input class="fc" type="text" name="digitall" placeholder="Qty per type">
