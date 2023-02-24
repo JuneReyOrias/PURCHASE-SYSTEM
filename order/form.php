16e22bc03e4adb392f67a0fefbe45346953f1269
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-24 11:36:27 +0800

    Update form.php

--- order/form.php
+++ order/form.php
@@ -158,8 +158,7 @@
         <div>
         <input class="fc" type="text" name="digitall" placeholder="Qty per type">
         <input class="form-control" id="formFileSm" type="file" />
-        <input class="fc" type="text" name="digitall" placeholder="Size">  
-</div>
+        <input class="fc" type="text" name="digitall" placeholder="Size">
         <label class="design"for=""></i>Printing type</label>
         <select name="Printing type "class="select multiple-select" multiple >
         
