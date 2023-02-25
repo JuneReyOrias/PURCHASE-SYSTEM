d770ff2c557cfa73275c09bbe104cf08a4202655
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-25 23:21:31 +0800

    Update createusers.php

--- admin/createusers.php
+++ admin/createusers.php
@@ -19,117 +19,76 @@ require_once '../database/user.class.php';
     require_once '../includes/header.php';
     
 ?>
-    
-   
-    
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-
-    <div class="home-content">
-        <div class="table-container">
-            <div class="table-heading">
-            <form class="select-forms" action="custinsert.php" method="post">
-                <h3 class="table-title">Faculty Profile</h3>
-                
-                    <a href="addaccount.php" class="button">Add New Faculty</a>
-              
-            
-            </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Academic Rank</th>
-                        <th>Department</th>
-                        <th>Admission Role</th>
-                        <th>Status</th>
-                        <?php
-                            if($_SESSION['user_type'] == 'admin'){ 
-                        ?>
-                            <th class="action">Action</th>
-                        <?php
-                            }
-                        ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        require_once '../database/user.class.php';
-
-                        $users = new users();
-                        //We will now fetch all the records in the array using loop
-                        //use as a counter, not required but suggested for the table
-                        $i = 1;
-                        //loop for each record found in the array
-                        $output= $user -> login(); //start of loop
-                    ?>
-                        <tr>
-                            <!-- always use echo to output PHP values -->
-                            <td><?php echo $i ?></td>
-                            <td><?php echo $value['lastname'] . ', ' . $value['firstname'] ?></td>
-                            <td><?php echo $value['email'] ?></td>
-                            <td><?php echo $value['academic_rank'] ?></td>
-                            <td><?php echo $value['department'] ?></td>
-                            <td><?php echo $value['admission_role'] ?></td>
-                            <td><?php echo $value['status'] ?></td>
-                            <?php
-                                if($_SESSION['user_type'] == 'admin'){ 
-                            ?>
-                                <td>
-                                    <div class="action">
-                                        <a class="action-edit" href="#">Edit</a>
-                                        <a class="action-delete" href="#">Delete</a>
-                                    </div>
-                                </td>
-                            <?php
-                                }
-                            ?>
-                        </tr>
-                    <?php
-                        $i++;
-                    //end of loop
-                    
-                    ?>
-                </tbody>
-            </table>
-                            </form>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">PHP CRUD OPERATION</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a type="button" class="btn btn-primary nav-link active" href="create.php">Add New</a>
+            </li>
+          </ul>
         </div>
-    </div>
-
-?>
-
-<script>
-        const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
+      </div>
+    </nav>
+    <div class="container my-4">
+    <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>PHONE</th>
+        <th>JOINING DATE</th>
+        <th>ACTIONS</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        include "connection.php";
+        $sql = "select * from crud2";
+        $result = $conn->query($sql);
+        if(!$result){
+          die("Invalid query!");
+        }
+        while($row=$result->fetch_assoc()){
+          echo "
+      <tr>
+        <th>$row[id]</th>
+        <td>$row[name]</td>
+        <td>$row[email]</td>
+        <td>$row[phone]</td>
+        <td>$row[join_date]</td>
+        <td>
+                  <a class='btn btn-success' href='edit.php?id=$row[id]'>Edit</a>
+                  <a class='btn btn-danger' href='delete.php?id=$row[id]'>Delete</a>
+                </td>
+      </tr>
+      ";
+        }
+      ?>
+    </tbody>
+  </table>
+      </div>
     
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
-});
-    </script>
-
-   
-    <script src="./js/sidebar.js"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
+Footer
