239eaa20bd091c44a2eee1e24312f99af6be2a3b
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-25 20:05:41 +0800

    Update customers.php

--- admin/customers.php
+++ admin/customers.php
@@ -154,37 +154,29 @@
         
         <div class="home-content">
         <form class="select-forms" action="custinsert.php" method="post">
-	<div class="row">
-		<div class="col-sm-8 ">
-        <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      
-			<table class="table table-bordered table-striped" style="margin-top:12px;" d>
-				<thead>
-					<th>ID</th>
-					<th>Firstname</th>
-					<th>Lastname</th>
-					<th>Address</th>
-                    <th>Firstname</th>
-					<th>Lastname</th>
-					<th>Address</th>
-					<th>Action</th>
-				</thead>
-				<tbody>
-</div>
-<div class="modal-footer">
+        ...
+      </div>
+      <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
+</div>
 </form>
 				
 
