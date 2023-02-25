0d1b5fe53b02d34913da3745b635bb54f7de55db
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-25 17:28:18 +0800

    Update customer.php

--- admin/customer.php
+++ admin/customer.php
@@ -1,4 +1,159 @@
-<div class="container">
+<?php
+
+    //resume session here to fetch session values
+    session_start();
+    /*
+        if user is not login then redirect to login page,
+        this is to prevent users from accessing pages that requires
+        authentication such as the dashboard
+    */
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+    //if the above code is false then html below will be displayed
+
+   
+    $page_title = 'Upress | Dashboard';
+    $dashboard = 'active';
+
+    require_once '../includes/header.php';
+    
+?>
+    
+   
+    </main> -->
+   
+
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                <img src="../img/upress-logo.png" alt="">
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">University</span>
+                    <span class="profession">Press</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                
+
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="dashboard.php">
+                            <i class='bx bxs-dashboard icon' ></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                        
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="items.php">
+                            <i class='bx bx-shopping-bag icon' ></i>
+                            <span class="text nav-text">Items</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="services.php">
+                            <i class='bx bx-printer icon' ></i>
+                            <span class="text nav-text">Services</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="customer.php">
+                            <i class='bx bx-user icon' ></i>
+                            <span class="text nav-text">Customer</span>
+                        </a>
+                    </li>
+
+                   
+                    <li class="nav-link">
+                        <a href="orders.php">
+                            <i class='bx bx-cart icon' ></i>
+                            <span class="text nav-text">Orders</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="container">
+    <div class="row profile">
+		<div class="col-md-3">
+			<div class="profile-sidebar">
+				<!-- SIDEBAR USERPIC -->
+				<div class="profile-userpic">
+					<img class ="rounded-circle img-fluid" src="../img/maderal.jpg" class="img-responsive" alt="">
+          <div class="profile-usertitle">
+					<div class="profile-usertitle-name">
+						<a href="#">Joe Maderal</a>
+					</div>
+				
+				</div>
+		
+				</div>
+            <div class="bottom-content">
+                <li class="">
+                    <a href="../login/login.php">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
+
+
+       
+    </section>
+
+    <section class="home">
+    <header>
+            <div class="image-logo">
+                <span >
+                <img src="../img/upress-logo.png" >
+                </span>
+
+                <div class="text">
+                    <span class="name">UNIVERSITY PRESS</span>
+                  
+                </div>
+                </div>
+
+           
+        </header>
+   
+        <hr class= "divider">
+        <div class="tapnav">
+        <a class="active" href="dashboard.php">Home</a>
+    
+</div>
+<div class="col1" ><span>Customer</span></div>
+        
+        <div class="home-content">
+        <div class="container">
    <br />
    <br />
    <h2 align="center">Ajax Crud on Dynamically Add Remove Input Fields in PHP</h2><br />
@@ -39,44 +194,46 @@
  </div>
 
 </div>
-<div class="container">
-   <br />
-   <br />
-   <h2 align="center">Ajax Crud on Dynamically Add Remove Input Fields in PHP</h2><br />
-   <div align="right">
-    <button type="button" name="add" id="add" class="btn btn-info">Add</button>
-   </div>
-   <br />
-   <div id="result"></div>
-  </div>
- </body>
-</html>
 
-<div id="dynamic_field_modal" class="modal fade" role="dialog">
- <div class="modal-dialog">
-  <div class="modal-content">
-   <form method="post" id="add_name">
-    <div class="modal-header">
-     <button type="button" class="close" data-dismiss="modal">&times;</button>
-     <h4 class="modal-title">Add Details</h4>
-    </div>
-    <div class="modal-body">
-     <div class="form-group">
-           <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" />
-          </div>
-          <div class="table-responsive">
-           <table class="table" id="dynamic_field">
+ <div>
 
-           </table>
-          </div>
-    </div>
-    <div class="modal-footer">
-     <input type="hidden" name="hidden_id" id="hidden_id" />
-     <input type="hidden" name="action" id="action" value="insert" />
-     <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
-    </div>
-   </form>
-  </div>
  </div>
+          
+    </section>
 
-</div>
+</nav>
+
+
+<script>
+        const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
+    </script>
+
+   
+    <script src="./js/sidebar.js"></script>
+  </body>
+</html>
