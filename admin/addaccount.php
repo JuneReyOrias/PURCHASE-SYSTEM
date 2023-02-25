3ff58de46587c1df420d752c6d99f85012e3e70f
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 01:37:35 +0800

    x

--- admin/addaccount.php
+++ admin/addaccount.php
@@ -1,115 +1,35 @@
 <?php
-require_once '../database/user.class.php';
-    //resume session here to fetch session values
-    session_start();
-    /*
-        if user is not login then redirect to login page,
-        this is to prevent users from accessing pages that requires
-        authentication such as the dashboard
-    */
-    if (!isset($_SESSION['logged-in'])){
-        header('location: ../login/login.php');
+session_start();
+require_once '../database/database.php';
+
+if(isset($_POST['save_student_btn']))
+{
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $course = $_POST['course'];
+
+    $query = "INSERT INTO students (fullname, email, phone, course) VALUES (:fullname, :email, :phone, :course)";
+    $query_run = $conn->prepare($query);
+
+    $data = [
+        ':fullname' => $fullname,
+        ':email' => $email,
+        ':phone' => $phone,
+        ':course' => $course,
+    ];
+    $query_execute = $query_run->execute($data);
+
+    if($query_execute)
+    {
+        $_SESSION['message'] = "Inserted Successfully";
+        header('Location: student-add.php');
+        exit(0);
     }
-    //if the above code is false then html below will be displayed
-
-   
-    $page_title = 'Upress | Dashboard';
-    $dashboard = 'active';
-
-    require_once '../includes/header.php';
-    
-?>
-<?php
-require_once '../database/user.class.php';
-    //resume session here to fetch session values
-    session_start();
-    /*
-        if user is not login then redirect to login page,
-        this is to prevent users from accessing pages that requires
-        authentication such as the dashboard
-    */
-    if (!isset($_SESSION['logged-in'])){
-        header('location: ../login/login.php');
+    else
+    {
+        $_SESSION['message'] = "Not Inserted";
+        header('Location: student-add.php');
+        exit(0);
     }
-    //if the above code is false then html below will be displayed
-
-   
-    $page_title = 'Upress | Dashboard';
-    $dashboard = 'active';
-
-    require_once '../includes/header.php';
-    
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">PHP CRUD OPERATION</a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a type="button" class="btn btn-primary nav-link active" href="create.php">Add New</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container my-4">
-    <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>NAME</th>
-        <th>EMAIL</th>
-        <th>PHONE</th>
-        <th>JOINING DATE</th>
-        <th>ACTIONS</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-        include "connection.php";
-        $sql = "select * from crud2";
-        $result = $conn->query($sql);
-        if(!$result){
-          die("Invalid query!");
-        }
-        while($row=$result->fetch_assoc()){
-          echo "
-      <tr>
-        <th>$row[id]</th>
-        <td>$row[name]</td>
-        <td>$row[email]</td>
-        <td>$row[phone]</td>
-        <td>$row[join_date]</td>
-        <td>
-                  <a class='btn btn-success' href='edit.php?id=$row[id]'>Edit</a>
-                  <a class='btn btn-danger' href='delete.php?id=$row[id]'>Delete</a>
-                </td>
-      </tr>
-      ";
-        }
-      ?>
-    </tbody>
-  </table>
-      </div>
-    
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>
-Footer
+}
--- admin/custfetch.php
+++ admin/custfetch.php
@@ -1,41 +1,72 @@
-
-<?php
-
-require_once '../database/database.php';
-require_once '../databse/user.class.php';
-    ?>
-<?php
- $connect = mysqli_connect('localhost', 'u151063784_upresswmsuprs', 'LadQw702+&3', 'u151063784_db_upress');
-    $query = "SELECT * FROM user_acc ORDER BY id DESC";
-    $result = mysqli_query($connect, $query);
+<?php 
+include('../database/database.php');
 ?>
-<!DOCTYPE html>
-<html>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Fetch data from database using pdo in php</title>
+  </head>
+  <body>
+    
     <div class="container">
-        <table class="table text-center table-striped table-bordered">
-            <tr>
-            <th>Username</th>
-              <th>Password</th>
-                <th>Fistname</th>
-                     <th>LastName</th>
-                      <th>Email</th>
-                        <th>Role</th>
-            </tr>
-            <?php 
-                while($row = mysqli_fetch_array($result)){
-                    echo "<tr>";
-                        echo "<td>".$row["user_name"]."</td>";
-                        echo "<td>".$row["password"]."</td>";
-                        echo "<td>".$row["firstname"]."</td>";
-                        echo "<td>".$row["lastname"]."</td>";
-                        echo "<td>".$row["email"]."</td>";
-                        echo "<td>".$row["role"]."</td>";
-                       
-                    echo "</tr>";
-                }
-            ?>
-        </table>
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Fetch data from database using pdo in php </h3>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>FullName</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Course</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $query = "SELECT * FROM user_acc";
+                                    $statement = $conn->prepare($query);
+                                    $statement->execute();
+
+                                    $statement->setFetchMode(PDO::FETCH_OBJ); //PDO::FETCH_ASSOC
+                                    $result = $statement->fetchAll();
+                                    if($result)
+                                    {
+                                        foreach($result as $row)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?= $row->id; ?></td>
+                                                
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                        <tr>
+                                            <td colspan="5">No Record Found</td>
+                                        </tr>
+                                        <?php
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
