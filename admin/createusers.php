7014454a96755c031ba59760daf96ec2440072e7
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 02:50:37 +0800

    Update createusers.php

--- admin/createusers.php
+++ admin/createusers.php
@@ -51,7 +51,7 @@ require_once '../database/database.php';
                     </div>
                     <div class="card-body">
                         
-                        <form action="createusers.php" method="POST">
+                        <form action="addaccount.php" method="POST">
                             <div class="mb-3">
                                 <label>username</label>
                                 <input type="text" name="user_name" class="form-control" />
