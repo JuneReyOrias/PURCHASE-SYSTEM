d6a0e9fccdc42b39b37c74db06d2519840a2e6f9
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 04:00:12 +0800

    s

--- admin/addcustomer.php
+++ admin/addcustomer.php
@@ -1,71 +1,147 @@
 <?php
-include 'connect.php';
-if(isset($_POST['submit'])){
-  $firstname=$_POST['firstname'];
-  $lastname=$_POST['lastname'];
-  $email=$_POST['email'];
-  $mobile=$_POST['mobile'];
-  $password=$_POST['password'];
-  $department=$_POST['department'];
+require_once '../database/database.php';
+require_once '../database/user.class.php';
 
-  $sql="insert into'"
-}
+    //resume session here to fetch session values
+    session_start();
+    /*
+        if user is not login then redirect to login page,
+        this is to prevent users from accessing pages that requires
+        authentication such as the dashboard
+    */
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+    //if the above code is false then html below will be displayed
+
+   
+    $page_title = 'Upress | Dashboard';
+    $dashboard = 'active';
+
+    require_once '../includes/header.php';
+    $obj= new Database();
+   
+    
+?>
+<?php 
+
+  
+if(isset($_POST['submit'])) {
+          //Sanitizing the inputs of the users. Mandatory to prevent injections!
+          print_r($_POST);
+              $user= new users;
+              $user -> username = htmlentities($_POST['user_name']); 
+              $user -> password = htmlentities($_POST['password']);
+              $user -> firstname = htmlentities($_POST['firstname']);
+
+              $user -> lastname = htmlentities($_POST['lastname']);
+
+              $user -> email = htmlentities($_POST['email']);
+      
+              $user -> role = htmlentities($_POST['role']);
+             
+              $output= $user -> create_acc();
+      
+              if ($output) {
+                  // CREATE -- COLUMN "firstname" "lastname" "role"
+                  print_r($output);
+
+                  $userData = $user -> login();
+
+                  if($userData) {
+                       $_SESSION['username'] = $userData['username'];
+                  $_SESSION['password'] = $userData['user_pass'];
+                  $_SESSION['fullname'] = $userData['firstname']  . ' ' . $userData['lastname'] . ' ' .
+                  $_SESSION['user_role'] = $userData['role'];
+                  $_SESSION['email'] = $userData['email'];
+                 
+                 
+            
+                  if($userData['role'] == 'Admin'){
+                    header('location: ../admin/dashboard.php');
+                  } else{
+                    header('location: ../user/user-profile.php');
+                    header('location: ../admin/dashboard1.php');
+                }
+                  }
+               
+      
+                  //display the appropriate dashboard page for user
+             
+                  }
+              
+        }
+  
+
+
+
+
+
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
-
-    <title>add customer</title>
-  </head>
-  <body>
-    <div class=" container">
-    <form method="post">
-  <div class="form-group">
-    <label>Firstname</label>
-    <input type="text" class="form-control"  placeholder="Enter your FirstName" name="fisrtname" autocomplete="off">
-  </div>
-  <div class="form-group">
-    <label>Lastname</label>
-    <input type="text" class="form-control"  placeholder="Enter your LastName" name="lastname" >
-  </div>
-  <div class="form-group">
-    <label>Email</label>
-    <input type="email" class="form-control"  placeholder="Enter your email" name="email" >
-  </div>
-  <div class="form-group">
-    <label>Mobile number</label>
-    <input type="text" class="form-control"  placeholder="Enter your mobile number" name="mobile" >
-  </div>
-  <div class="form-group">
-    <label>password</label>
-    <input type="text" class="form-control"  placeholder="Enter your password" name="password" >
-  </div>
   
-  <div class="form-group">
-  <label> Department </label></br>
 
-    <input list="department" name="department">
-  <datalist id="department">
-    <option value="CCS">
-    <option value="CTE">
-    <option value="Architecture">
-    <option value="CCJE">
-    <option value="COE">
-    <option value="CLA">
-  </datalist>
-  
+<div class="signup-container">
+    <div class="title">Sign Up Form</div>
+    
+    <div class="content">
+
+    <!--PERSONAL INFORMATION-->
+    <div class="sub-title">Personal Information</div>
+      <form action="addcustomer.php" method="post">
+
+        <div class="user-details">
+        <div class="input-box">
+            <span class="details">Username</span>
+            <input type="text" placeholder="" id="user_name" name="user_name" placeholder="user_name" required tabindex="1"> 
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="text" placeholder="" id="password" name="password" placeholder="password" required tabindex="1"> 
+          </div>
+
+          <div class="input-box">
+            <span class="details">Firstname</span>
+            <input type="text" placeholder="" id="firstname" name="firstname" placeholder="Enter firstname" required tabindex="1"> 
+          </div>
+
+         
+          <div class="input-box">
+            <span class="details">Lastname</span>
+            <input type="text"  id="firstname" name="lastname" placeholder="Enter lastname" required tabindex="3">
+          </div>
+
+         
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="text"  id="email" name="email" placeholder="Enter email" required tabindex="4">
+          </div>
+          
+          
+         <div class="input-box">
+          <span class="details">Role</span>
+          <select name="role" id="role" required>
+            <option value="None">--Select--</option>
+            <option value="SuperAdmin" > Admin</option>
+            <option value="Admin">Cutomer</option>
+            <option value="Users">Staff</option>
+          </select>
+      </div>
+          
+         
+
+    
+
+
+        
+         <!--SIGN UP BUTTON-->
+        <div class="button">
+          <input type="submit" action="signup.php" name="submit" id="submit" value="Sign Up">
+        </div>
+        <div class="have-account">
+        <p>ALREADY HAVE AN ACCOUNT?<span> <a href="login.php">LOG IN</a></span></p>
+        </div>
+      </form>
+    </div>
   </div>
-  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-</form>
-</div>
-
- 
-  </body>
-</html>
