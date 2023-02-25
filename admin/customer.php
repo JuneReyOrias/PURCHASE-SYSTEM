d36de9c22afb887f443f27dadbbac37c0f2521b5
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-25 18:33:12 +0800

    dd

--- admin/customer.php
+++ admin/customer.php
@@ -152,23 +152,73 @@
 </div>
 <div class="col1" ><span>Customer</span></div>
      
-        <div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
+<div class="container">
+	<h1 class="page-header text-center">PHP CRUD Operation using PDO</h1>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> New</a>
+            <?php 
+                session_start();
+                if(isset($_SESSION['message'])){
+                    ?>
+                    <div class="alert alert-info text-center" style="margin-top:20px;">
+                        <?php echo $_SESSION['message']; ?>
+                    </div>
+                    <?php
+ 
+                    unset($_SESSION['message']);
+                }
+            ?>
+			<table class="table table-bordered table-striped" style="margin-top:20px;">
+				<thead>
+					<th>ID</th>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					<th>Address</th>
+					<th>Action</th>
+				</thead>
+				<tbody>
+					<?php
+						//include our connection
+						include_once('connection.php');
+ 
+						$database = new Connection();
+    					$db = $database->open();
+						try{	
+						    $sql = 'SELECT * FROM members';
+						    foreach ($db->query($sql) as $row) {
+						    	?>
+						    	<tr>
+						    		<td><?php echo $row['id']; ?></td>
+						    		<td><?php echo $row['firstname']; ?></td>
+						    		<td><?php echo $row['lastname']; ?></td>
+						    		<td><?php echo $row['address']; ?></td>
+						    		<td>
+						    			<a href="#edit_<?php echo $row['id']; ?>" class="btn btn-success btn-sm" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+						    			<a href="#delete_<?php echo $row['id']; ?>" class="btn btn-danger btn-sm" data-toggle="modal"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+						    		</td>
+						    		<?php include('edit_delete_modal.php'); ?>
+						    	</tr>
+						    	<?php 
+						    }
+						}
+						catch(PDOException $e){
+							echo "There is some problem in connection: " . $e->getMessage();
+						}
+ 
+						//close connection
+						$database->close();
+ 
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
+<?php include('add_modal.php'); ?>
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
  <div>
 
  </div>
@@ -209,5 +259,6 @@ modeSwitch.addEventListener("click" , () =>{
 
    
     <script src="./js/sidebar.js"></script>
-  </body>
-</html>
+<?php
+require_once'../include/footer1.php';
+?>
