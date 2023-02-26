be970ad76d3a0bf6c135bd9a8a4f5c02a7a5dfc9
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-27 07:21:48 +0800

    Update create_user.php

--- admin/create_user.php
+++ admin/create_user.php
@@ -8,8 +8,19 @@
         <a href="indexk.php">Contact</a>
         <a href='create_user.php'>CreateUsers</a>
 </div>
+
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> UPPRES </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+</head>
+<body>
 
 
  
