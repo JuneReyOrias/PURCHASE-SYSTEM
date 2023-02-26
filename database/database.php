e78a50b275ce8230750ce0e0fb4e3d565289936d
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 11:08:25 +0800

    Update database.php

--- database/database.php
+++ database/database.php
@@ -30,7 +30,7 @@ public function inserRecord($post){
    $lastname=$post['lastname'];
    $email=$post['email'];
    $role=$post['role'];
-   $sql = "INSERT INTO `user_acc`( `user_name`, `user_pass`, `firstname`, `lastname`, `email`, `role`) VALUES ('$username','$password','$firstname','$lastname','$email','$role')";
+   $sql = "INSERT INTO `user_acc`( `user_name`, `user_pass`, `firstname`, `lastname`, `email`, `role`) VALUES ('','','','','','')";
    $result=$this->connect()->query($sql);
    if($result){
     header('location: addcustomer.php?msg=');
