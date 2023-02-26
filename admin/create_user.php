82f92725d961c52f5b287b0a6652ab7ada99540e
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-27 07:17:04 +0800

    ddd

--- admin/create_user.php
+++ admin/create_user.php
@@ -11,16 +11,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> UPPRES </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
-</head>
-<body>
-
+<div class="home-content">
+    <div class="select-container">              
+            <form class="select-form" action="sample.php" method="post">
     <!-- Modal -->
     <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
@@ -258,7 +251,7 @@
                 </div>
             </div>
 
-
+</form>
         </div>
     </div>
 
