6d7a9db17aef76dc09ce6520c62b0255a48276dd
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-27 06:56:10 +0800

    d

--- admin/create_user.php
+++ admin/create_user.php
@@ -151,19 +151,9 @@
         <a href="indexk.php">Contact</a>
         <a href='create_user.php'>CreateUsers</a>
 </div>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> UPPRES </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
-</head>
-<body>
-<div class="home-container">
+<div class="home-content">
+    <div class="table-container">
     <!-- Modal -->
     <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
@@ -497,7 +487,7 @@
         });
     </script>
 
-
+</html>
 
  </div>
           
--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
 
 
 
