f122f90f82f070a44017bead40efab4735d5ef4b
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 09:48:07 +0800

    d

--- database/database.php
+++ database/database.php
@@ -5,14 +5,14 @@ class Database{
     public $username = 'u151063784_upresswmsuprs';
     public $password = 'LadQw702+&3';
     public $database = 'u151063784_db_upress';
-    protected $conn;
+    protected $connection;
 
     function connect(){
         try 
 			{
-				$this->conn = new mysqli("mysql:host=$this->host;dbname=$this->database", 
+				$this->connection = new mysqli("mysql:host=$this->host;dbname=$this->database", 
 											$this->username, $this->password);
-              if($this->conn->connect_error){
+              if($this->connection->connect_error){
                 echo 'Connection Failed';
                 
                }else{
