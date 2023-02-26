6cfaac637693e77a87ce5010a8bd8b6235201262
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-27 07:56:48 +0800

    Update dashboard.php

--- customer/dashboard.php
+++ customer/dashboard.php
@@ -1,111 +1,105 @@
 <?php
 
-    //resume session here to fetch session values
-    session_start();
-    /*
-        if user is not login then redirect to login page,
-        this is to prevent users from accessing pages that requires
-        authentication such as the dashboard
-    */
-    if (!isset($_SESSION['logged-in'])){
-        header('location: ../login/login.php');
-    }
-    //if the above code is false then html below will be displayed
+//resume session here to fetch session values
+session_start();
+/*
+    if user is not login then redirect to login page,
+    this is to prevent users from accessing pages that requires
+    authentication such as the dashboard
+*/
+if (!isset($_SESSION['logged-in'])){
+    header('location: ../login/login.php');
+}
+//if the above code is false then html below will be displayed
 
-   
-    $page_title = 'Upress | Dashboard';
-    $dashboard = 'active';
 
-    require_once '../includes/header.php';
+$page_title = 'Upress | Dashboard';
+$dashboard = 'active';
+
+require_once '../includes/header.php';
 ?>
-    
-   
-    </main> -->
-   
-    <nav class="sidebar close">
-        <header>
-            <div class="image-text">
-                <span class="image">
-                <img src="../img/upress-logo.png" alt="">
-                </span>
 
-                <div class="text logo-text">
-                    <span class="name">University</span>
-                    <span class="profession">Press</span>
-                </div>
-            </div>
 
-            <i class='bx bx-chevron-right toggle'></i>
-        </header>
 
-        <div class="menu-bar">
-            <div class="menu">
 
-            
+<nav class="sidebar close">
+    <header>
+        <div class="image-text">
+            <span class="image">
+            <img src="../img/upress-logo.png" alt="">
+            </span>
 
-                <ul class="menu-links">
-                    <li class="nav-link">
-                        <a href="../customer/dashboard.php">
-                            <i class='bx bxs-dashboard icon' ></i>
-                            <span class="text nav-text">Dashboard</span>
-                        </a>
-                        
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="../purchase/purchases.php">
-                            <i class='bx bx-shopping-bag icon' ></i>
-                            <span class="text nav-text">Purchase</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="../order/order.php">
-                            <i class='bx bx-cart icon' ></i>
-                            <span class="text nav-text">Orders</span>
-                        </a>
-                    </li>
-
-                   
-                    <li class="nav-link">
-                        <a href="../trackordr/trackorder.php">
-                            <i class='bx bx-map icon' ></i>
-                            <span class="text nav-text">Track Orders</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-            <div class="container">
-    <div class="row profile">
-		<div class="col-md-3">
-			<div class="profile-sidebar">
-				<!-- SIDEBAR USERPIC -->
-				<div class="profile-userpic">
-					<img class ="rounded-circle img-fluid" src="../img/maderal.jpg" class="img-responsive" alt="">
-          <div class="profile-usertitle">
-					<div class="profile-usertitle-name">
-						<a href="#">Joe Maderal</a>
-					</div>
-				
-				</div>
-		
-				</div>
-            <div class="bottom-content">
-                <li class="">
-                    <a href="../login/login.php">
-                        <i class='bx bx-log-out icon' ></i>
-                        <span class="text nav-text">Logout</span>
-                    </a>
-                </li>
-
-             
-                </li>
-                
+            <div class="text logo-text">
+                <span class="name">University</span>
+                <span class="profession">Press</span>
             </div>
         </div>
 
-    </nav>
+        <i class='bx bx-chevron-right toggle'></i>
+    </header>
+
+    <div class="menu-bar">
+        <div class="menu">
+
+
+            <ul class="menu-links">
+                <li class="nav-link">
+                    <a href=".dashboard.php">
+                        <i class='bx bxs-dashboard icon' ></i>
+                        <span class="text nav-text">Dashboard</span>
+                    </a>
+                    
+                </li>
+
+                <li class="nav-link">
+                    <a href="../purchase/purchases.php">
+                        <i class='bx bx-shopping-bag icon' ></i>
+                        <span class="text nav-text">Purchase</span>
+                    </a>
+                </li>
+
+                <li class="nav-link">
+                    <a href="../order/order.php">
+                        <i class='bx bx-cart icon' ></i>
+                        <span class="text nav-text">Orders</span>
+                    </a>
+                </li>
+
+               
+                <li class="nav-link">
+                    <a href="../trackordr/trackorder.php">
+                        <i class='bx bx-map icon' ></i>
+                        <span class="text nav-text">Track Orders</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+        <div class="bottom-content">
+            <li class="">
+                <a href="../login/login.php">
+                    <i class='bx bx-log-out icon' ></i>
+                    <span class="text nav-text">Logout</span>
+                </a>
+            </li>
+
+            <li class="mode">
+                <div class="sun-moon">
+                    <i class='bx bx-moon icon moon'></i>
+                    <i class='bx bx-sun icon sun'></i>
+                </div>
+                <span class="mode-text text">Dark mode</span>
+
+                <div class="toggle-switch">
+                    <span class="switch"></span>
+                </div>
+            </li>
+            
+        </div>
+    </div>
+
+</nav>
 
        
     </section>
