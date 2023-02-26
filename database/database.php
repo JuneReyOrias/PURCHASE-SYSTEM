edc2ec6273ac2896f93e684fe2347697adbf381d
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-26 09:39:19 +0800

    c

--- database/database.php
+++ database/database.php
@@ -5,21 +5,22 @@ class Database{
     public $username = 'u151063784_upresswmsuprs';
     public $password = 'LadQw702+&3';
     public $database = 'u151063784_db_upress';
-    protected $connection;
+    protected $conn;
 
     function connect(){
         try 
 			{
-				$this->connection = new PDO("mysql:host=$this->host;dbname=$this->database", 
+				$this->conn = new mysqli("mysql:host=$this->host;dbname=$this->database", 
 											$this->username, $this->password);
-                    
-			} 
-			catch (PDOException $e) 
-			{
-				echo "Connection error " . $e->getMessage();
-			}
-        return $this->connection;
-    }
+              if($this->conn->connect_error){
+                echo 'Connection Failed';
+                
+               }else{
+                echo 'Connected';
+               }
+
+            }
+        }
 
 }
 
