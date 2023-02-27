e3c1737597b8f5cf9aab0a33df94b6acc09c4d10
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-27 13:30:06 +0800

    Update create_user.php

--- admin/create_user.php
+++ admin/create_user.php
@@ -352,7 +352,7 @@
                 $query = "SELECT * FROM user_acc";
                 $query_run = mysqli_query($connection, $query);
             ?>
-                    <table id="datatableid" class="table table-bordered table-dark">
+                    <table id="datatableid" class="table table-bordered table-light">
                         <thead>
                             <tr>
                                 <th scope="col"> ID</th>
