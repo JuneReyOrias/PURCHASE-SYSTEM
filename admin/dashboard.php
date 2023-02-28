f34807928deb7d768696b95915577aadba13d6db
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 08:31:16 +0800

    rrr

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -17,107 +17,16 @@
     $dashboard = 'active';
 
     require_once '../includes/header.php';
-    
+    require_once '../includes/sidebar.php';
 ?>
     
    
     
 
-    <nav class="sidebar close">
-        <header>
-            <div class="image-text">
-                <span class="image">
-                <img src="../img/upress-logo.png" alt="">
-                </span>
-
-                <div class="text logo-text">
-                    <span class="name">University</span>
-                    <span class="profession">Press</span>
-                </div>
-            </div>
-
-            <i class='bx bx-chevron-right toggle'></i>
-        </header>
-
-        <div class="menu-bar">
-            <div class="menu">
-
-                
-
-                <ul class="menu-links">
-                    <li class="nav-link">
-                        <a href="#">
-                            <i class='bx bxs-dashboard icon' ></i>
-                            <span class="text nav-text">Dashboard</span>
-                        </a>
-                        
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="addcustomer.php">
-                            <i class='bx bx-shopping-bag icon' ></i>
-                            <span class="text nav-text">Items</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="services.php">
-                            <i class='bx bx-printer icon' ></i>
-                            <span class="text nav-text">Services</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-link">
-                        <a href="customers.php">
-                            <i class='bx bx-user icon' ></i>
-                            <span class="text nav-text">Customer</span>
-                        </a>
-                    </li>
-
-                   
-                    <li class="nav-link">
-                        <a href="orders.php">
-                            <i class='bx bx-cart icon' ></i>
-                            <span class="text nav-text">Orders</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-         
-			<div class="profile-sidebar">
-				<!-- SIDEBAR USERPIC -->
-				<div class="profile-userpic">
-					<img class ="rounded-circle img-fluid" src="../img/maderal.jpg" class="img-responsive" alt="">
-          <div class="profile-usertitle">
-					<div class="profile-usertitle-name">
-						<a href="#">Joe Maderal</a>
-					</div>
-				
-				</div>
-		
-				</div>
-            <div class="bottom-content">
-                <li class="">
-                    <a href="../login/login.php">
-                        <i class='bx bx-log-out icon' ></i>
-                        <span class="text nav-text">Logout</span>
-                    </a>
-                </li>
-
-              
-                  
-                
-            </div>
-        </div>
-
-    </nav>
-
-
        
-    </section>
+ 
 
-    <section class="home">
+    
     <header>
             <div class="image-logo">
                 <span >
@@ -174,41 +83,5 @@
 
  </div>
           
-    </section>
-
-</nav>
 
 
-<script>
-        const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
-});
-    </script>
-
-   
-    <script src="./js/sidebar.js"></script>
-  </body>
-</html>
