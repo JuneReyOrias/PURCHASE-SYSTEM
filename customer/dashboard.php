8cd13ad6383d0c866935885c1be8750511bdfa6b
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 17:43:59 +0800

    d

--- customer/dashboard.php
+++ customer/dashboard.php
@@ -29,7 +29,7 @@ left:25px; " >
                 <div class="text-center" style= "color: #990000;">
                 <img src="/img/upress-logo.png" class="rounded mx-auto d-block" alt="UPRESS">
                 <span class="text-center fs-1">UNIVERSITY PRESS</span>
-               <hr class="divider"
+               <hr class="divider">
                   
                 </div>
                
