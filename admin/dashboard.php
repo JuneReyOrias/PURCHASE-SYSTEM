2efea0f4fbbfa0de59bd99b3af97a54d8e24a7d2
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 12:39:20 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -82,7 +82,12 @@ left:22px;" >
  </div>
    
  <div>
- <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    
+ </div>
+</section>
+
+<section>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -120,5 +125,4 @@ left:22px;" >
     <span class="visually-hidden">Next</span>
   </button>
 </div>
- </div>
 </section>
