d24842374fd1263fbb7223c50d8197e1823e49dd
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-01 07:06:06 +0800

    Update adprofile.php

--- admin/adprofile.php
+++ admin/adprofile.php
@@ -55,7 +55,7 @@ left:25px;" >
       <div class="col-lg-4">
         <div class="card mb-4">
           <div class="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+            <img src="../img/maderal.jpg" alt="avatar"
               class="rounded-circle img-fluid" style="width: 150px;">
             <h5 class="my-3">Admin Joe M</h5>
             <p class="text-muted mb-1">Full Stack Developer</p>
