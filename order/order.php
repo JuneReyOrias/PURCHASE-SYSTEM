5e841e111614d6027176c1517162187b346974d2
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 22:58:53 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -56,7 +56,7 @@ left:25px; " >
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
@@ -65,7 +65,25 @@ left:25px; " >
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+    
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+    
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
