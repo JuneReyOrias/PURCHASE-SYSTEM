0e6f39bf95c3de8f665661ed092dec2e506f0db2
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-01 04:45:02 +0800

    Update dashboard.php

--- customer/dashboard.php
+++ customer/dashboard.php
@@ -52,7 +52,7 @@ left:25px; " >
         
 <div class="row">
   <div class="col-sm-3">
-    <div class="card"style="  box-shadow: 0 14px 16px #990000;border-radius: 12px; height:10rem;" >
+    <div class="card"style=" margin-top:2rem; box-shadow: 0 14px 16px #990000;border-radius: 12px; height:10rem;" >
       <div class="card-body">
         <h5 class="card-title">Printing</h5>
         <a href="#" class="btn btn-primary">1243</a>
@@ -61,7 +61,7 @@ left:25px; " >
     </div>
   </div>
   <div class="col-sm-3">
-    <div class="card"style="  box-shadow: 0 14px 16px #990000;border-radius: 12px; height:10rem;" >
+    <div class="card"style="  margin-top:2rem;box-shadow: 0 14px 16px #990000;border-radius: 12px; height:10rem;" >
       <div class="card-body">
         <h5 class="card-title">Binding</h5>
         <a href="#" class="btn btn-primary">467</a>
