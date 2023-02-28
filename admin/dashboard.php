eec886bed74c9c42288e2e4bb23c8f96beae31e9
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 10:07:57 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -50,9 +50,9 @@
         
         <div class="home-content">
         <div class="overview-boxes" >
-            <div class="box">
+            <div class="box ">
                 <div class="right-side">
-                    <div class="box-topic" class="btn-1">Printing</div> </br>
+                    <div class="box-topic ms-lg-3" class="btn-1">Printing</div> </br>
                     <div class="number">Printing is a process for mass reproducing text and images using a master form or template.</div>
                     
                     </div>
