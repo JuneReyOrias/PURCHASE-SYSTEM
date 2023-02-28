556be8c417a63eab569c24da2e1fe8b0ae35597f
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 20:55:26 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -62,26 +62,7 @@ left:25px; " >
                     </div>
                 </div>
 
-                <div class="box">
-                <div class="center">
-                    <div class="box-topic" class="btn-1">Layout</div></br>
-                    <div class="number">The act or process of planning or laying out in detail.</div>
-                    
-                    </div>
-                </div>
-                
-               
-            <div class="box">
-                <div class="left-side">
-                    <div class="box-topic" class="btn-1">Binding</div></br>
-                    <div class="number">A strong covering holding the pages of a book together</div>
-                    
-                    </div>
-                    
-                </div>
-
-                </div>
-
+         
                 
     
 
