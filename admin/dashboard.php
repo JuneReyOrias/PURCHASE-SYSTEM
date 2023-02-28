efe21400f0e74e4085c144e10dbe7c579e4c35ff
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 12:49:11 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -47,93 +47,7 @@ left:22px;" >
 </div>
 </div>
        
-        
-        <div class="home-content">
-        <div class="overview-boxes ms-lg-5" >
-            <div class="box ms-lg-5 " style= "margin-left:23px">
-                <div class="right-side">
-                    <div class="box-topic ms-lg-3" class="btn-1">Printing</div> </br>
-                    <div class="number">Printing is a process for mass reproducing text and images using a master form or template.</div>
-                    
-                    </div>
-                </div>
-
-                <div class="box">
-                <div class="center">
-                    <div class="box-topic" class="btn-1">Layout</div></br>
-                    <div class="number">The act or process of planning or laying out in detail.</div>
-                    
-                    </div>
-                </div>
-                
-               
-            <div class="box">
-                <div class="left-side">
-                    <div class="box-topic" class="btn-1">Binding</div></br>
-                    <div class="number">A strong covering holding the pages of a book together</div>
-                    
-                    </div>
-                    
-                </div>
-
-                <div class="box">
-                <div class="left-side">
-                    <div class="box-topic" class="btn-1">Binding</div></br>
-                    <div class="number">A strong covering holding the pages of a book together</div>
-                    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-                    </div>
-                    
-                </div>
-
-                
- <div>
-    
-
- </div>
-   
- <div>
-    
- </div>
- <div class="carousel-indicators">
+<div class="carousel-indicators">
     <button
       type="button"
       data-mdb-target="#carouselBasicExample"
@@ -199,5 +113,51 @@ left:22px;" >
 </div>
 <!-- Carousel wrapper -->
 
+        <div class="home-content">
+        <div class="overview-boxes ms-lg-5" >
+            <div class="box ms-lg-5 " style= "margin-left:23px">
+                <div class="right-side">
+                    <div class="box-topic ms-lg-3" class="btn-1">Printing</div> </br>
+                    <div class="number">Printing is a process for mass reproducing text and images using a master form or template.</div>
+                    
+                    </div>
+                </div>
+
+                <div class="box">
+                <div class="center">
+                    <div class="box-topic" class="btn-1">Layout</div></br>
+                    <div class="number">The act or process of planning or laying out in detail.</div>
+                    
+                    </div>
+                </div>
+                
+               
+            <div class="box">
+                <div class="left-side">
+                    <div class="box-topic" class="btn-1">Binding</div></br>
+                    <div class="number">A strong covering holding the pages of a book together</div>
+                    
+                    </div>
+                    
+                </div>
+
+                <div class="box">
+                <div class="left-side">
+                    <div class="box-topic" class="btn-1">Binding</div></br>
+                    <div class="number">A strong covering holding the pages of a book together</div>
+                    
+                    </div>
+                    
+                </div>
+
+                
+ <div>
+    
+
+ </div>
+   
+ <div>
+    
+ </div>
 </section>
 
