78ae01ced995d357dcc0549384805662d292e1bf
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 21:21:18 +0800

    Update order.php

--- order/order.php
+++ order/order.php
@@ -54,7 +54,7 @@ left:25px; " >
         
             <div class="home-content">
             <div class="card">
-            
+            <div class="overview-boxes ms-lg-5" >
             <div class="box ms-lg-5 "         style= "margin-left:23px">
                 <div class="right-side">
                     <div class="box-topic ms-lg-3" class="btn-1">Printing</div> </br>
