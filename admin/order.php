83859a42566659037715319a9437aab1cc240c95
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-01 06:12:41 +0800

    dd

---
+++ admin/order.php
@@ -0,0 +1,398 @@
+<?php
+
+    //resume session here to fetch session values
+    session_start();
+    /*
+        if user is not login then redirect to login page,
+        this is to prevent users from accessing pages that requires
+        authentication such as the dashboard
+    */
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+    //if the above code is false then html below will be displayed
+
+   
+    $page_title = 'Upress | Dashboard';
+    $dashboard = 'active';
+
+    require_once '../includes/header.php';
+    require_once '../includes/sidebar.php';
+?>
+<section class="home-dash ms-5 "style="height: 160vh;
+    width: 100%;background-color: var(--body-color);
+left:25px;" >
+<div>
+        <header>
+
+
+                <div class="text-center" style= "color: #990000;">
+                <img src="/img/upress-logo.png" class="rounded mx-auto d-block" alt="UPRESS">
+                <span class="text-center fs-1">UNIVERSITY PRESS</span>
+
+                  
+                </div>
+               
+
+           
+        </header>
+   
+        <hr class= "divider">
+        <div class="tapnav">
+        <a class="lefte ms-lg-3" href="dashboard.php">Home</a>
+        <a href="#contact">Admin</a>
+        <a href='#contact us'>Hola!</a>
+        <a href='create_user.php'>CreateUsers</a>
+        <div class="ser-center fs-2 " >
+
+        <span>Create Items </span></div> 
+</div>
+</div>
+
+
+
+      
+    <!-- Modal -->
+    <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Create Account </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="insertcode.php" method="POST">
+
+                    <div class="modal-body">
+                        <div class="form-group">
+                        <label>Username </label>
+                            <input type="text" name="user_name" id="user_name" class="form-control"
+                                placeholder="Enter UserName">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Password </label>
+                            <input type="text" name="user_pass" id="user_pass" class="form-control"
+                                placeholder="Enter Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Firstname </label>
+                            <input type="text" name="firstname" id="firstname" class="form-control"
+                                placeholder="Enter Firstname">
+                        </div>
+                        <div class="form-group">
+                            <label> lastname </label>
+                            <input type="text" name="lastname" id="lastname" class="form-control"
+                                placeholder="Enter Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label> Email </label>
+                            <input type="text" name="email" id="email" class="form-control"
+                                placeholder="Enter Fullname">
+                        </div>
+                        <div class="form-group">
+                            <label> role </label>
+                            <input type="text" name="role" id="role" class="form-control"
+                                placeholder="Enter Role">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" name="insertdata" class="btn btn-primary">Save Data</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Edit Customer Accounts </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="updatecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="update_id" id="update_id">
+
+                        <div class="form-group">
+                            <label> Password </label>
+                            <input type="text" name="user_pass" id="user_pass" class="form-control"
+                                placeholder="Enter Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Firstname </label>
+                            <input type="text" name="firstname" id="firstname" class="form-control"
+                                placeholder="Enter Firstname">
+                        </div>
+                        <div class="form-group">
+                            <label> lastname </label>
+                            <input type="text" name="lastname" id="lastname" class="form-control"
+                                placeholder="Enter Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label> Email </label>
+                            <input type="text" name="email" id="email" class="form-control"
+                                placeholder="Enter Fullname">
+                        </div>
+                        <div class="form-group">
+                            <label> role </label>
+                            <input type="text" name="role" id="role" class="form-control"
+                                placeholder="Enter Role">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Delete Account </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="deletecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="delete_id" id="delete_id">
+
+                        <h4> Do you want to Delete this Account ??</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"> NO </button>
+                        <button type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- VIEW POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="viewmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> View Customer Accounts </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="deletecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="text" name="view_id" id="view_id">
+
+                        <!-- <p id="fname"> </p> -->
+                        <h4 id="user_name"> <?php echo ''; ?> </h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"> CLOSE </button>
+                        <!-- <button type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button> -->
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" >
+       
+                <h2> Purchase </h2>
+           
+
+
+
+                <?php
+                $connection = mysqli_connect('localhost','u151063784_upresswmsuprs','LadQw702+&3');
+                $db = mysqli_select_db($connection, 'u151063784_db_upress');
+
+                $query = "SELECT * FROM purchase_order";
+                $query_run = mysqli_query($connection, $query);
+            ?>
+                    <table id="datatableid" class="table table-bordered table-dark table-responsive"class="table table-responsive">
+                        <thead>
+                            <tr>
+                                <th scope="col">OrderId</th>
+                                <th scope="col">OrderDate </th>
+                                <th scope="col">BuyerId </th>
+                                <th scope="col">requireDate </th>
+                                <th scope="col">Expe_deliverDate</th>
+                                <th scope="col">paymentId </th>
+                                <th scope="col"> Action </th>
+                                <th scope="col"> Action </th>
+                                
+                                
+                            </tr>
+                        </thead>
+                        <?php
+                if($query_run)
+                {
+                    foreach($query_run as $row)
+                    {
+            ?>
+                        <tbody>
+                            <tr>
+                                <td > <?php echo $row['ordr_id']; ?> </td>
+                                <td> <?php echo $row['ordr_date']; ?> </td>
+                                <td> <?php echo $row['buyer_id']; ?> </td>
+                                <td> <?php echo $row['req_date']; ?> </td>
+                                <td> <?php echo $row['exp_deliver_date']; ?> </td>
+                                <td> <?php echo $row['payment_id']; ?> </td>
+                              
+                              
+                             
+                                <td>
+                                    <button type="button" class="btn btn-success editbtn"> Approved  </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger deletebtn"> Disapproved </button>
+                                </td>  
+                            </tr>
+                        </tbody>
+                        <?php           
+                    }
+                }
+                else 
+                {
+                    echo "No Record Found";
+                }
+            ?>
+                    </table>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('.viewbtn').on('click', function () {
+                $('#viewmodal').modal('show');
+                $.ajax({ //create an ajax request to display.php
+                    type: "GET",
+                    url: "display.php",
+                    dataType: "html", //expect html to be returned                
+                    success: function (response) {
+                        $("#responsecontainer").html(response);
+                        //alert(response);
+                    }
+                });
+            });
+
+        });
+    </script>
+
+
+   <!-- <script>
+        $(document).ready(function () {
+
+            $('#datatableid').DataTable({
+                "pagingType": "full_numbers",
+                "lengthMenu": [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, "All"]
+                ],
+                responsive: true,
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search Your Data",
+                }
+            });
+
+        });
+    </script>-->
+
+    <script>
+        $(document).ready(function () {
+
+            $('.deletebtn').on('click', function () {
+
+                $('#deletemodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+
+            });
+        });
+    </script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('.editbtn').on('click', function () {
+
+                $('#editmodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#update_id').val(data[0]);
+                $('#user_name').val(data[1]);
+                $('#user_pass').val(data[2]);
+                $('#firstname').val(data[3]);
+                $('#lastname').val(data[4]);
+                $('#email').val(data[5]);
+                $('#role').val(data[6]);
+            });
+        });
+    </script>
+
+
+
+ 
