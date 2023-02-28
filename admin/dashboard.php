b866283a19634968658f49827eb66b2a82258c3d
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-02-28 13:29:00 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,7 +19,7 @@
     require_once '../includes/header.php';
     require_once '../includes/sidebar.php';
 ?>
-<section class="home-dash ms-lg-3"style=" background-color: var(--body-color);
+<section class="home-dash ms-lg-3"style="margin-left:3rem; background-color: var(--body-color);
 left:22px;" >
 <div>
         <header>
