d57179c8a0e9908b06330013e289ecbc87f28024
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-02 07:28:56 +0800

    Update orderlist.php

--- order/orderlist.php
+++ order/orderlist.php
@@ -291,6 +291,11 @@ left:25px; " >
                                
                             </tr>
                         </tbody>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-2" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+</div>
+
                         <?php           
                     }
                 }
