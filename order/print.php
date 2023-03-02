7c40df25df221da4e8ef8b3b6ceecd375daf59c7
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-02 10:37:13 +0800

    Update print.php

--- order/print.php
+++ order/print.php
@@ -45,8 +45,8 @@ left:25px; " >
     <div class="tapnav">
     <  <a class="lefte ms-lg-3" href="addcustomer.php">Home</a>
     <a href="">Shop</a>
-    <a class="order" href="order.php" ><span>></span>Order</a>
-    <a class="order" href="order.php" ><span>></span>Printing</a><br>
+    <a class="order" href="order.php" >Order</a>
+    <a class="order" href="print.php" ><span>></span>PrintingForm</a><br>
     <div class="ser-center fs-2 " >
          <span class="ser-center fs-2 " ></span></div> 
 </div> 
