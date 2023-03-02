3946f190b7f7cf3f911064f860084422c8e5084f
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-02 18:08:25 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -85,7 +85,7 @@
                     
                 </div>
 
-                <div class="box d-flex justify-content-center">
+                <div class="box">
                 <div class="left-side">
                     <div class="box-topic" class="btn-1">Binding</div></br>
                     <div class="number">A strong covering holding the pages of a book together</div>
