f59b15470db6491de9fcc403599884b42fd37b99
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-02 17:18:26 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -78,14 +78,15 @@
 
                
                
-            <div class="box">
-                <div class="left-side">
-                    <div class="box-topic" class="btn-1">Binding</div></br>
-                    <div class="number">A strong covering holding the pages of a book together</div>
-                    
-                    </div>
-                    
-                </div>
+                <div class="col-sm-3">
+    <div class="card"style="  margin-top:3rem;box-shadow: 0 14px 16px #990000;border-radius: 12px; height:10rem;">
+      <div class="card-body">
+        <h5 class="card-title">Lamination</h5>
+    
+        <a href="lamination.php" class="btn btn-primary">Continue</a><br>
+      </div>
+    </div>
+  </div>
                
 
                 
