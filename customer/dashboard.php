35d392f49114cf8ef138b24c51d3d163114f55b0
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-02 16:33:38 +0800

    Update dashboard.php

--- customer/dashboard.php
+++ customer/dashboard.php
@@ -41,7 +41,7 @@ left:25px; " >
         <div class="tapnav">
         <a class="lefte ms-lg-3" href="addcustomer.php">Home</a>
         <a class="lefte ms-lg-3" href="order.php">Shop</a><br>
-        <div class="ser-center fs-2 " >
+        <div class="ser-center fs-2 " > 
 
         <span class="text-center fs-1">Services</span></div> 
 </div>
