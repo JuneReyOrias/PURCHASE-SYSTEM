6b52127d4ea92abcb93cf0cabb9831d5f102bb55
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-02 20:37:08 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="shortcut icon" type="x-icon" href="upress-logo.png">
+    <link rel="icon" type="image/png" href="upress-logo.png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=p, initial-scale=1.0">
