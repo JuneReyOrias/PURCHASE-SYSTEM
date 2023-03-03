db4731f7658757f962f5c85177107c927ded6d70
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 11:13:56 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -60,7 +60,7 @@
 <body>
 
 <div class="container">
-<div classs="row">
+<div classs="row ms-3">
   <h2>Carousel Example</h2>
   <div id="myCarousel" class="carousel slide"style="width:40%;float:center;" data-ride="carousel">
     <!-- Indicators -->
