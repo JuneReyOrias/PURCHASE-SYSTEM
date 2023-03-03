0b94b95efec3537c326ab5ebfe87cabed9ff1374
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 13:18:49 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -50,7 +50,7 @@
 </body>
 <div class="conatainer">
  
- <div class="multi-carousel-inner float-end" >
+ <div class="multi-carousel-inner float-end" style="margin-right:23px;" >
      <div class="multi-carousel-item">
          <div class="card m-2">
              <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Square/img(4).jpg" alt="Card image cap" />
