5700bef93b4a432352ca51c1d632c1d67d4a6f0b
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 13:49:15 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,7 +19,7 @@
     require_once '../includes/header.php';
     require_once '../includes/sidebar.php';
 ?>
-<section class="home-dash ms-5"style="left:25px;" >
+<section class="home-dash ms-5"style="left:29px;" >
 <div>
         <header>
 
