6a6267c10acfced568cce4306d93e10c81fb9891
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 08:49:55 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,94 +48,44 @@
 <body class="human " style="background-color: var(--body-color);
     transition: var(--tran-05);">
    
-<!-- Carousel wrapper -->
-<div id="carouselExampleControls" class="carousel slide text-center carousel-dark" data-mdb-ride="carousel">
+   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="rounded-circle shadow-1-strong mb-4"
-        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp" alt="avatar"
-        style="width: 150px;" />
-      <div class="row d-flex justify-content-center">
-        <div class="col-lg-8">
-          <h5 class="mb-3">Maria Kate</h5>
-          <p>Photographer</p>
-          <p class="text-muted">
-            <i class="fas fa-quote-left pe-2"></i>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti
-            nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia
-            fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit
-            doloremque.
-          </p>
-        </div>
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
       </div>
-      <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="far fa-star fa-sm"></i></li>
-      </ul>
     </div>
     <div class="carousel-item">
-      <img class="rounded-circle shadow-1-strong mb-4"
-        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar"
-        style="width: 150px;" />
-      <div class="row d-flex justify-content-center">
-        <div class="col-lg-8">
-          <h5 class="mb-3">John Doe</h5>
-          <p>Web Developer</p>
-          <p class="text-muted">
-            <i class="fas fa-quote-left pe-2"></i>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti
-            nesciunt sint eligendi reprehenderit reiciendis.
-          </p>
-        </div>
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
       </div>
-      <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="far fa-star fa-sm"></i></li>
-      </ul>
     </div>
     <div class="carousel-item">
-      <img class="rounded-circle shadow-1-strong mb-4"
-        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp" alt="avatar" style="width: 150px;" />
-      <div class="row d-flex justify-content-center">
-        <div class="col-lg-8">
-          <h5 class="mb-3">Anna Deynah</h5>
-          <p>UX Designer</p>
-          <p class="text-muted">
-            <i class="fas fa-quote-left pe-2"></i>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti
-            nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia
-            fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit
-            doloremque.
-          </p>
-        </div>
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
       </div>
-      <ul class="list-unstyled d-flex justify-content-center text-warning mb-0">
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="fas fa-star fa-sm"></i></li>
-        <li><i class="far fa-star fa-sm"></i></li>
-      </ul>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleControls"
-    data-mdb-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleControls"
-    data-mdb-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-<!-- Carousel wrapper -->
 
 
 
