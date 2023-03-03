0ac25dfc4efd00072aa2ae413c53b2b14894746c
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 08:25:46 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -49,7 +49,15 @@
     transition: var(--tran-05);">
    
 
-   
+   class="multi-carousel" data-mdb-interval="3000" data-mdb-items="5">
+    <div class="d-flex justify-content-center m-2 mb-3">
+        <button class="carousel-control-prev btn btn-pink btn-floating mx-3" type="button" tabindex="0" data-mdb-slide="prev">
+            <i class="fas fa-angle-left fa-lg"></i>
+        </button>
+        <button class="carousel-control-next btn btn-pink btn-floating mx-3" type="button" tabindex="0" data-mdb-slide="next">
+            <i class="fas fa-angle-right fa-lg"></i>
+        </button>
+    </div>
 
 
     <div class="multi-carousel-inner"style="margin-left:20rem;">
