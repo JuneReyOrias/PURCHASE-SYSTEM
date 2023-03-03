20f3e98662107ee91970796a8b4b1f8ef3449e8c
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 09:16:10 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -45,50 +45,12 @@
         <span></span></div> 
 </div>
 </div>
-<body class="human " style="background-color: var(--body-color);
+<body class="human " style="background-color: var(--body-color) height;70vh;
     transition: var(--tran-05);">
    
 
    
 
-<!-- Home Section Start  -->
-<section class="home">
-
-   <div class="swiper home-slider">
-      <div class="swiper-wrapper">
-         <!-- PHSI Carousel Start -->
-         <?php 
-         require_once 'classes/carousel_model.php';
-         $carousel = new Carousel();
-         //We will now fetch all the records in the array using loop
-         //use as a counter, not required but suggested for the table
-         $i = 1;
-         //loop for each record found in the array
-         foreach ($carousel->fetchAllRecords() as $value) { //start of loop
-         $carousel_img = $value['filename'];
-         $caousel_head = $value['carousel_title'];
-         $carousel_body = $value['carousel_content'] ;
-         ?>
-         <section class="swiper-slide slide"  style="background: url('uploads/<?php echo $carousel_img; ?>') no-repeat;">
-            <div class="content" id="unesco_content">
-               <h3><?php echo $value['carousel_title'] ?></h3>
-               <p><?php echo $value['carousel_content'] ?></p>
-               <!--<a href="#" class="btn">Read more</a> -->
-            </div>
-         </section>
-         <?php
-         $i++;
-      }
-      //end loop
-  ?>
-         <!-- PHSI Carousel End -->
-      </div>
-      <!--Next/Prev Carousel Button-->
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
-      <!--Current Carousel 3 dots-->
-      <div class="swiper-pagination"></div>
-   </div>
 
 
 </body>
