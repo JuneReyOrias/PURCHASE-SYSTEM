238f71d3305ba14db16c39b2fa40d5b972205d51
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 09:04:33 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -133,7 +133,40 @@
       <div class="swiper-button-prev swiper-navBtn"></div>
       <div class="swiper-pagination"></div>
     </div>
+<script>
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});</script>
  
     <script src="./js/sidebar.js"></script>
     
