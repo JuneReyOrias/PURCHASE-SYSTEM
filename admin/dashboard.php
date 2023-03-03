d5370718de9c8a4a2bf83c2594533ff784ca019f
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 11:58:08 +0800

    d

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -47,17 +47,7 @@
 </div>
 <body class="human " style="background-color: var(--body-color);
     transition: var(--tran-05);">
-   
-   <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+</body>
 
 <div class="container style=margin-left:10rem;">
 
@@ -163,8 +153,5 @@
   </div>
 </div>
 
-</div>
 
-
-</body>
 </section>
