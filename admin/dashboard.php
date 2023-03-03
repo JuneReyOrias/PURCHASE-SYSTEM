da1b3bef5527dbe37f87b11584884ff14d6cead6
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 13:30:09 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,7 +48,7 @@
 <body class="human " style="background-color: var(--body-color);
     transition: var(--tran-05); ">
 </body>
-<div class="card-group">
+<div class="card-group floa">
   <div class="card">
     <img class="card-img-top" src="https://api.metroprint.ph/storage/product-preset-categories/OAQ9pd7BeD31Poe/preset-394bed0faadce20ef547ced4275be82b.webp" alt="Card image cap">
     <div class="card-body">
@@ -73,6 +73,15 @@
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
+  <div class="card">
+    <img class="card-img-top" src="https://api.metroprint.ph/storage/product-preset-categories/OAQ9pd7BeD31Poe/preset-394bed0faadce20ef547ced4275be82b.webp" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
 </div>
 
 <div class="container style=margin-left:10rem;">
