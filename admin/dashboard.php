49df9c9c7f81ebba76e358228046e1b7dbd6eb6e
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-03 14:10:51 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -148,7 +148,7 @@
   </div>
  
 <h2>Carousel Example</h2>
-<div id="myCarousel" class="carousel slide float-md-center"style="idth:100%; height:50vh;margin-left:50px;" data-ride="carousel">
+<div id="myCarousel" class="carousel slide float-md-center"style="width:100%; height:50vh;margin-left:50px;" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators"style="width:50%;float:center;">
     <li data-target="#myCarousel" data-slide-to="0" class="active"style="width:50%;"></li>
