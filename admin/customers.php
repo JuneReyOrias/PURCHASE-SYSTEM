197e62332a7f6d669aef6d7cdad64d40fb46198e
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-06 19:12:00 +0800

    Update customers.php

--- admin/customers.php
+++ admin/customers.php
@@ -1,7 +1,8 @@
 <?php
-   require_once 'database/user.class.php';
+   
     //resume session here to fetch session values
     session_start();
+    require_once '../database/user.class.php';
     /*
         if user is not login then redirect to login page,
         this is to prevent users from accessing pages that requires
