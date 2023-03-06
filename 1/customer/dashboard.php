19c3cb31443e3d3e31650139c804ad6018dd3ed0
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-06 21:37:26 +0800

    Update dashboard.php

--- 1/customer/dashboard.php
+++ 1/customer/dashboard.php
@@ -162,6 +162,13 @@ require_once '../includes/cust.sidebar.php';
       </div>
     </div>
   
+    <div class="item">
+      <img src="https://api.metroprint.ph/storage/product-preset-categories/GKlRbDv5lLrBa2p/preset-1c1876ceb4829ddef89befa5b773e544.webp" alt="New York"style="width:100%;height:50vh">
+      <div class="carousel-caption">
+        <h3>ID Holder</h3>
+        <p>Price</p>
+      </div>
+    </div>
 
   </div>
 
