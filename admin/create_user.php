e92545446dd798163f35338296fa87ccad03923a
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-08 16:29:46 +0800

    Update create_user.php

--- admin/create_user.php
+++ admin/create_user.php
@@ -226,7 +226,7 @@ left:25px;" >
     </div>
 
 
-            <div class="card">
+            <div>
                
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal"style="margin-left: 35px;">
                         ADD New User
