7fde59f298576e9ba09370255fe7fbe836abf4ab
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-08 16:54:23 +0800

    Update insercode.php

--- admin/insercode.php
+++ admin/insercode.php
@@ -1,21 +1,10 @@
 <?php
-
-$connection = mysqli_connect("localhost",'u151063784_upresswmsuprs','LadQw702+&3');
-$db = mysqli_select_db($connection, 'u151063784_db_upress');
-
+require_once '../database/user.class.php';
 
 if(isset($_POST['insertdata']))
 {
-    $user_name = $_POST['user_name'];
-    $user_pass = $_POST['user_pass'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $role = $_POST['role'];
-
-    $query = "INSERT INTO user_acc (`user_name`,`user_pass`,`firstname`, `lastname`, `email`,`role`) VALUES ('$user_name','$user_pass','$firstname','$lastname', '$email', '$role')";
-    $query_run = mysqli_query($connection, $query);
-
+   $user= new Users;
+   $user->add();
     if($query_run)
     {
         echo '<script> alert("Data Saved"); </script>';
