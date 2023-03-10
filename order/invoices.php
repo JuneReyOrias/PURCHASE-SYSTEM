2c0a293eaa4b72b905917d0162265f47fdcf1b4f
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-10 21:28:47 +0800

    Create invoices.php

---
+++ order/invoices.php
@@ -0,0 +1,61 @@
+<?php
+
+    //resume session here to fetch session values
+    session_start();
+    /*
+        if user is not login then redirect to login page,
+        this is to prevent users from accessing pages that requires
+        authentication such as the dashboard
+    */
+    if (!isset($_SESSION['logged-in'])){
+        header('location: ../login/login.php');
+    }
+    //if the above code is false then html below will be displayed
+
+   
+    $page_title = 'Upress | Dashboard';
+    $dashboard = 'active';
+
+    require_once '../includes/header.php';
+    require_once '../includes/cust.sidebar.php';
+?>
+    
+   
+
+
+
+       
+ 
+
+    <section class="home-dash ms-5 ms-5"style="left:25px;">
+<div>
+        <header>
+
+
+                <div class="text-center" style= "color: #990000;">
+                <img src="/img/upress-logo.png" class="rounded mx-auto d-block" alt="UPRESS">
+                <span class="text-center fs-1">UNIVERSITY PRESS</span>
+               <hr class="divider">
+                  
+                </div>
+               
+
+           
+        </header>
+   
+      
+        <div class="tapnav">
+         <a class="lefte ms-lg-3" href="dashboard.php">Home</a>
+        <a href="">Shop</a>
+        <a class="order" href="order.php" ><span></span>Order</a>
+        <a class="order" href="binding.php" ><span></span>BindingForm</a>
+        <a class="order" href="orderlist.php" ><span>></span>OrderList</a><br>
+
+        </div>
+<body class="human " style="background-color: var(--body-color);
+    transition: var(--tran-05);"> 
+       
+  <div>
+    <input type="text" name="orderid" placeholder="">
+  </div>
+  
