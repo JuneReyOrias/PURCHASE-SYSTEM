161be27cd36747d40b0730a3a25e67b63420faef
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-15 22:32:04 +0800

    Update track.php

--- trackordr/track.php
+++ trackordr/track.php
@@ -228,47 +228,99 @@ left:25px; " >
     </div>
 
 
-    <section class="vh-100" style="background-color: #eee;">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col">
-        <div class="card card-stepper" style="border-radius: 10px;">
-          <div class="card-body p-4">
+    import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="d-flex flex-column">
-                <span class="lead fw-normal">Your order has been delivered</span>
-                <span class="text-muted small">by DHFL on 21 Jan, 2020</span>
-              </div>
-              <div>
-                <button class="btn btn-outline-primary" type="button">Track order details</button>
-              </div>
-            </div>
-            <hr class="my-4">
+export default function OrderDetails6() {
+  return (
+    <>
+      <section className="vh-100" style={{ backgroundColor: "#8c9eff" }}>
+        <MDBContainer className="py-5 h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol size="12">
+              <MDBCard
+                className="card-stepper text-black"
+                style={{ borderRadius: "16px" }}
+              >
+                <MDBCardBody className="p-5">
+                  <div className="d-flex justify-content-between align-items-center mb-5">
+                    <div>
+                      <MDBTypography tag="h5" className="mb-0">
+                        INVOICE{" "}
+                        <span className="text-primary font-weight-bold">
+                          #Y34XDHR
+                        </span>
+                      </MDBTypography>
+                    </div>
+                    <div className="text-end">
+                      <p className="mb-0">
+                        Expected Arrival <span>01/12/19</span>
+                      </p>
+                      <p className="mb-0">
+                        USPS{" "}
+                        <span className="font-weight-bold">
+                          234094567242423422898
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <ul
+                    id="progressbar-2"
+                    className="d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2"
+                  >
+                    <li className="step0 active text-center" id="step1"></li>
+                    <li className="step0 active text-center" id="step2"></li>
+                    <li className="step0 active text-center" id="step3"></li>
+                    <li className="step0 text-muted text-end" id="step4"></li>
+                  </ul>
 
-            <div class="d-flex flex-row justify-content-between align-items-center align-content-center">
-              <span class="dot"></span>
-              <hr class="flex-fill track-line"><span class="dot"></span>
-              <hr class="flex-fill track-line"><span class="dot"></span>
-              <hr class="flex-fill track-line"><span class="dot"></span>
-              <hr class="flex-fill track-line"><span
-                class="d-flex justify-content-center align-items-center big-dot dot">
-                <i class="fa fa-check text-white"></i></span>
-            </div>
-
-            <div class="d-flex flex-row justify-content-between align-items-center">
-              <div class="d-flex flex-column align-items-start"><span>15 Mar</span><span>Order placed</span>
-              </div>
-              <div class="d-flex flex-column justify-content-center"><span>15 Mar</span><span>Order
-                  placed</span></div>
-              <div class="d-flex flex-column justify-content-center align-items-center"><span>15
-                  Mar</span><span>Order Dispatched</span></div>
-              <div class="d-flex flex-column align-items-center"><span>15 Mar</span><span>Out for
-                  delivery</span></div>
-              <div class="d-flex flex-column align-items-end"><span>15 Mar</span><span>Delivered</span></div>
-            </div>
-
-          </div>
+                  <div className="d-flex justify-content-between">
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon fas icon="clipboard-list me-lg-4 mb-3 mb-lg-0" size="3x" />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Processed</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon fas icon="box-open me-lg-4 mb-3 mb-lg-0" size="3x" />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Shipped</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon fas icon="shipping-fast me-lg-4 mb-3 mb-lg-0" size="3x" />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">En Route</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon fas icon="home me-lg-4 mb-3 mb-lg-0" size="3x" />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Arrived</p>
+                      </div>
+                    </div>
+                  </div>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+    </>
+  );
+}
         </div>
       </div>
     </div>
