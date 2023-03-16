889bbea6cb489d65c9a3579387a53b3c79b8d108
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-17 06:50:02 +0800

    Update trackorder.php

--- trackordr/trackorder.php
+++ trackordr/trackorder.php
@@ -59,63 +59,154 @@
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Create Account </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+            <div class="container-fluid">
+	<div class="col-lg-12">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="callout callout-info">
+					<dl>
+						<dt>Tracking Number:</dt>
+						<dd> <h4><b><?php echo $reference_number ?></b></h4></dd>
+					</dl>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="callout callout-info">
+					<b class="border-bottom border-primary">Sender Information</b>
+					<dl>
+						<dt>Name:</dt>
+						<dd><?php echo ucwords($sender_name) ?></dd>
+						<dt>Address:</dt>
+						<dd><?php echo ucwords($sender_address) ?></dd>
+						<dt>Contact:</dt>
+						<dd><?php echo ucwords($sender_contact) ?></dd>
+					</dl>
+				</div>
+				<div class="callout callout-info">
+					<b class="border-bottom border-primary">Recipient Information</b>
+					<dl>
+						<dt>Name:</dt>
+						<dd><?php echo ucwords($recipient_name) ?></dd>
+						<dt>Address:</dt>
+						<dd><?php echo ucwords($recipient_address) ?></dd>
+						<dt>Contact:</dt>
+						<dd><?php echo ucwords($recipient_contact) ?></dd>
+					</dl>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="callout callout-info">
+					<b class="border-bottom border-primary">Parcel Details</b>
+						<div class="row">
+							<div class="col-sm-6">
+								<dl>
+									<dt>Wight:</dt>
+									<dd><?php echo $weight ?></dd>
+									<dt>Height:</dt>
+									<dd><?php echo $height ?></dd>
+									<dt>Price:</dt>
+									<dd><?php echo number_format($price,2) ?></dd>
+								</dl>	
+							</div>
+							<div class="col-sm-6">
+								<dl>
+									<dt>Width:</dt>
+									<dd><?php echo $width ?></dd>
+									<dt>length:</dt>
+									<dd><?php echo $length ?></dd>
+									<dt>Type:</dt>
+									<dd><?php echo $type == 1 ? "<span class='badge badge-primary'>Deliver to Recipient</span>":"<span class='badge badge-info'>Pickup</span>" ?></dd>
+								</dl>	
+							</div>
+						</div>
+					<dl>
+						<dt>Branch Accepted the Parcel:</dt>
+						<dd><?php echo ucwords($branch[$from_branch_id]) ?></dd>
+						<?php if($type == 2): ?>
+							<dt>Nearest Branch to Recipient for Pickup:</dt>
+							<dd><?php echo ucwords($branch[$to_branch_id]) ?></dd>
+						<?php endif; ?>
+						<dt>Status:</dt>
+						<dd>
+							<?php 
+							switch ($status) {
+								case '1':
+									echo "<span class='badge badge-pill badge-info'> Collected</span>";
+									break;
+								case '2':
+									echo "<span class='badge badge-pill badge-info'> Shipped</span>";
+									break;
+								case '3':
+									echo "<span class='badge badge-pill badge-primary'> In-Transit</span>";
+									break;
+								case '4':
+									echo "<span class='badge badge-pill badge-primary'> Arrived At Destination</span>";
+									break;
+								case '5':
+									echo "<span class='badge badge-pill badge-primary'> Out for Delivery</span>";
+									break;
+								case '6':
+									echo "<span class='badge badge-pill badge-primary'> Ready to Pickup</span>";
+									break;
+								case '7':
+									echo "<span class='badge badge-pill badge-success'>Delivered</span>";
+									break;
+								case '8':
+									echo "<span class='badge badge-pill badge-success'> Picked-up</span>";
+									break;
+								case '9':
+									echo "<span class='badge badge-pill badge-danger'> Unsuccessfull Delivery Attempt</span>";
+									break;
+								
+								default:
+									echo "<span class='badge badge-pill badge-info'> Item Accepted by Courier</span>";
+									
+									break;
+							}
 
-                <script>
-	function track_now(){
-		start_load()
-		var tracking_num = $('#ref_no').val()
-		if(tracking_num == ''){
-			$('#parcel_history').html('')
-			end_load()
-		}else{
-			$.ajax({
-				url:'ajax.php?action=get_parcel_heistory',
-				method:'POST',
-				data:{ref_no:tracking_num},
-				error:err=>{
-					console.log(err)
-					alert_toast("An error occured",'error')
-					end_load()
-				},
-				success:function(resp){
-					if(typeof resp === 'object' || Array.isArray(resp) || typeof JSON.parse(resp) === 'object'){
-						resp = JSON.parse(resp)
-						if(Object.keys(resp).length > 0){
-							$('#parcel_history').html('')
-							Object.keys(resp).map(function(k){
-								var tl = $('#clone_timeline-item .iitem').clone()
-								tl.find('.dtime').text(resp[k].date_created)
-								tl.find('.timeline-body').text(resp[k].status)
-								$('#parcel_history').append(tl)
-							})
-						}
-					}else if(resp == 2){
-						alert_toast('Unkown Tracking Number.',"error")
-					}
-				}
-				,complete:function(){
-					end_load()
-				}
-			})
-		}
+							?>
+							<span class="btn badge badge-primary bg-gradient-primary" id='update_status'><i class="fa fa-edit"></i> Update Status</span>
+						</dd>
+
+					</dl>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal-footer display p-0 m-0">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+</div>
+<style>
+	#uni_modal .modal-footer{
+		display: none
 	}
-	$('#track-btn').click(function(){
-		track_now()
-	})
-	$('#ref_no').on('search',function(){
-		track_now()
+	#uni_modal .modal-footer.display{
+		display: flex
+	}
+</style>
+<noscript>
+	<style>
+		table.table{
+			width:100%;
+			border-collapse: collapse;
+		}
+		table.table tr,table.table th, table.table td{
+			border:1px solid;
+		}
+		.text-cnter{
+			text-align: center;
+		}
+	</style>
+	<h3 class="text-center"><b>Student Result</b></h3>
+</noscript>
+<script>
+	$('#update_status').click(function(){
+		uni_modal("Update Status of: <?php echo $reference_number ?>","manage_parcel_status.php?id=<?php echo $id ?>&cs=<?php echo $status ?>","")
 	})
 </script>
-
-            </div>
-        </div>
-    </div>
 
     <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
     <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
