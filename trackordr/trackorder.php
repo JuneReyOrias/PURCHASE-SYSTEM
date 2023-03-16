77bd5bb91cb7e978c77cce219d0449aa1985c05f
Sidrick Cadungog <sidrickcadungogg@gmail.com>
2023-03-17 07:35:20 +0800

    Update trackorder.php

--- trackordr/trackorder.php
+++ trackordr/trackorder.php
@@ -66,32 +66,7 @@
                     </button>
                 </div>
 
-                
-<div class="col-lg-12">
-	<div class="card card-outline card-primary">
-		<div class="card-body">
-			<div class="d-flex w-100 px-1 py-2 justify-content-center align-items-center">
-				<label for="">Enter Tracking Number</label>
-				<div class="input-group col-sm-5">
-                    <input type="search" id="ref_no" class="form-control form-control-sm" placeholder="Type the tracking number here">
-                    <div class="input-group-append">
-                        <button type="button" id="track-btn" class="btn btn-sm btn-primary btn-gradient-primary">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-8 offset-md-2">
-			<div class="timeline" id="parcel_history">
-				
-			</div>
-		</div>
-	</div>
-</div>
-<div id="clone_timeline-item" class="d-none">
+                <div id="clone_timeline-item" class="d-none">
 	<div class="iitem">
 	    <i class="fas fa-box bg-blue"></i>
 	    <div class="timeline-item">
