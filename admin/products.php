1813a07213a4289e2e1d4e4c81c71d7fa06a0322
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-17 17:36:01 +0800

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -56,7 +56,7 @@ left:25px;" >
     transition: var(--tran-05);">
 
      <!-- Modal -->
-     <div class="modal fade" id="productaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -108,7 +108,7 @@ left:25px;" >
     </div>
 
     <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
-    <div class="modal fade" id="editproductmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="editcustomermodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -121,35 +121,45 @@ left:25px;" >
 
                 <form action="updatecode.php" method="POST">
 
-                    <div class="modal-body">
-
-                        <input type="hidden" name="update_id" id="update_id">
-
-                        <div class="modal-body">
-                        <div class="form-group">
-                        <label>ProductName </label>
-                            <input type="text" name="product_name" id="product_name" class="form-control"
-                                placeholder="Enter ProductName">
-                        </div>
-
-                        <div class="form-group">
-                            <label> ProductDescription </label>
-                            <input type="text" name="product_desc" id="product_desc" class="form-control"
-                                placeholder="Enter ProducDesc">
-                        </div>
-
-                        <div class="form-group">
-                            <label> UnitPrices </label>
-                            <input type="text" name="unit_price" id="unit_price" class="form-control"
-                                placeholder="Enter UnitPrice">
-                        </div>
-                        <div class="form-group">
-                            <label> Quantity</label>
-                            <input type="text" name="qty" id="qty" class="form-control"
-                                placeholder="Enter Qty">
-                        </div>
-                        
                    
+                <div class="modal-body">
+                        <div class="form-group">
+                        <label>Username </label>
+                            <input type="text" name="user_name" id="user_name" class="form-control"
+                                placeholder="Enter UserName">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Password </label>
+                            <input type="text" name="user_pass" id="user_pass" class="form-control"
+                                placeholder="Enter Password">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Firstname </label>
+                            <input type="text" name="firstname" id="firstname" class="form-control"
+                                placeholder="Enter Firstname">
+                        </div>
+                        <div class="form-group">
+                            <label> lastname </label>
+                            <input type="text" name="lastname" id="lastname" class="form-control"
+                                placeholder="Enter Lastname">
+                        </div>
+                        <div class="form-group">
+                            <label> Email </label>
+                            <input type="text" name="email" id="email" class="form-control"
+                                placeholder="Enter Email">
+                        </div>
+                        <div class="form-group">
+                            <label> ContactNo </label>
+                            <input type="text" name="contact_no" id="contact_no" class="form-control"
+                                placeholder="Enter ContactNo">
+                        </div>
+                        <div class="form-group">
+                            <label> role </label>
+                            <input type="text" name="role" id="role" class="form-control"
+                                placeholder="Enter Role">
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -163,33 +173,31 @@ left:25px;" >
     </div>
 
     <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
-    <div class="modal fade" id="deleteproductmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="deletecustomermodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"> Delete Product </h5>
+                    <h5 class="modal-title" id="exampleModalLabel"> Delete Account </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
 
-                <form action="deleteproduct.php" method="POST">
+                <form action="deletecode.php" method="POST">
 
                     <div class="modal-body">
 
-                        
+                        <input type="hidden" name="delete_id" id="delete_id">
 
-                        <h4> Do you want to Delete this Product ??</h4>
+                        <h4> Do you want to Delete this Account ??</h4>
                     </div>
                     <div class="modal-footer">
                                 <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
                                 <input type="hidden" name="action" value="delete">
-                                
+                                <input type="hidden" name="id" value="<?php echo $users['id']; ?>">
                                 <input type="submit" class="btn btn-danger" value="Delete">
                             </div>
-                                
-                              
                 </form>
 
             </div>
@@ -231,51 +239,51 @@ left:25px;" >
 
           
     <h2 style="margin-left: 40rem;"> Product </h2>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#productaddmodal"style="margin-left: 135px;">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal"style="margin-left: 135px;">
                         Add New Product
                     </button>
                 </div>
           
 
         
-
-                <table id="datatableid" class="table table-bordered table-responsive"style="margin-left:150px;">
-                        <thead class="thead-dark">
+                    <table id="datatableid" class="table table-bordered table-responsive"style="margin-left:150px;">
+                        <thead class="table-dark ">
                             <tr>
-                                <th scope="col"> ProductID</th>
-                                <th scope="col">ProductName </th>
-                                <th scope="col">ProductDesc </th>
-                                <th scope="col">Unit_Prices </th>
-                                <th scope="col"> Quantity </th>
-                                <th scope="col"> Action </th>
-                                
+                                <th scope="col"> ID</th>
+                                <th scope="col">Username </th>
+                                <th scope="col">Password </th>
+                                <th scope="col">Firstname </th>
+                                <th scope="col"> Lastname </th>
+                                <th scope="col"> Email </th>
+                                 <th scope="col"> Contact </th>
+                                <th scope="col"> Role </th>
+                                <th scope="col"> Action</th>
+                             
                             </tr>
                         </thead>
                         <?php
-                             $product= new ProductS();
-                             $productdata= $product->show();
-                             
-                             foreach($productdata as $product){ 
-    
+                         $user= new Users();
+                         $userdata= $user->show();
+                         
+                         foreach($userdata as $user){ 
 
             ?>
-                
                         <tbody>
                             <tr>
-                                <td > <?php echo $product['product_id']; ?> </td>
-                                <td> <?php echo $product['product_name']; ?> </td>
-                                <td> <?php echo $product['product_desc']; ?> </td>
-                                <td> <?php echo $product['unit_price']; ?> </td>
-                                <td> <?php echo $product['qty']; ?> </td>
-                               
+                                <td > <?php echo $user['id']; ?> </td>
+                                <td> <?php echo $user['user_name']; ?> </td>
+                                <td> <?php echo $user['user_pass']; ?> </td>
+                                <td> <?php echo $user['firstname']; ?> </td>
+                                <td> <?php echo $user['lastname']; ?> </td>
+                                <td> <?php echo $user['email']; ?> </td>
+                                <td> <?php echo $user['contact_no']; ?> </td>
+                                <td> <?php echo $user['role']; ?> </td>
                                 <td>
-                                  
-                                  
-                                    <button type="button" class="btn btn-success editbtn" data-toggle="modal" data-target="#editproductmodal"> EDIT </button>
-                                   <button type="button" class="btn btn-danger deletebtn" data-toggle="modal" data-target="#deleteproductmodal"> DELETE </button>
-            
+                                   
+                                <button type="button" class="btn btn-success editbtn" data-toggle="modal" data-target="#editcustomermodal"> EDIT </button>
+                                
+                                    <button type="button" class="btn btn-danger deletebtn" data-toggle="modal" data-target="#deletecustomermodal"> DELETE </button>
                                 </td>
-                              
                             </tr>
                         </tbody>
                         <?php   
