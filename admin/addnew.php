31bba3a212b72b49cb12f2f0293bc6e02f10c5dd
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-17 12:40:14 +0800

    Update addnew.php

--- admin/addnew.php
+++ admin/addnew.php
@@ -7,21 +7,15 @@ if (isset($_POST['action']) && $_POST['action'] == 'add') {
    
     $newuser = new Users();
     // Sanitize input data
-    $username = htmlspecialchars($_POST['user_name']);
-    $password = htmlspecialchars($_POST['user_pass']);
-    $firstname = htmlspecialchars($_POST['firstname']);
-    $lastname = htmlspecialchars($_POST['lastname']);
-    $email = htmlspecialchars($_POST['email']);
-    $contactNo = htmlspecialchars($_POST['contact_no']);
-    $role = htmlspecialchars($_POST['role']);
+
 
     
 
     // Check if any of the form fields are empty
-    if (empty($username)  || empty($firstname)||empty($lastname)||empty($email)||empty($contactNo)||empty($role)) {
-        echo 'All fields are required';
-        exit();
-    }
+    //if (empty($username)  || empty($firstname)||empty($lastname)||empty($email)||empty($contactNo)||empty($role)) {
+      //  echo 'All fields are required';
+      //  exit();
+//}
 
     
     // Add the fee to the database
