9812a85f2a23fdd18bfd06ab40999b7666452a4e
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-17 11:36:00 +0800

    Update insercode.php

--- admin/insercode.php
+++ admin/insercode.php
@@ -4,7 +4,7 @@ require_once "../database/user.class.php";
 
 // Check if the fosaverm has been submitted
 if (isset($_POST['insertdata']) && $_POST['insertdata'] == 'Save data') {
-    
+    print_r($_POST);
     // Sanitize input data
     $username = htmlspecialchars($_POST['user_name']);
     $password = htmlspecialchars($_POST['user_pass']);
@@ -27,11 +27,11 @@ if (isset($_POST['insertdata']) && $_POST['insertdata'] == 'Save data') {
 
     // Create a new Fee object and set its properties
     $user = new Users();
-   
+    $insertdata = $user->add();
     
     // Add the fee to the database
     if ($user->add()) {
-        header('location: customer.php');
+        header('location: customers.php');
     } else {
         echo 'Failed to add fee.';
     }
