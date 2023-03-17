8315ce40f1939038cd0b605aed4dd02e934ae0eb
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-17 13:48:21 +0800

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -97,8 +97,7 @@ left:25px;" >
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <input type="hidden" name="action" value="add">
-					<input type="submit" class="btn btn-success" value="add">
-                       
+					<input type="submit" class="btn btn-success" value="Add">
                     </div>
                 </form>
 
