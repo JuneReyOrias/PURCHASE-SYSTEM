5dc66d933e1c1dcf1b35f0d608647f98e1135c16
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-17 15:57:01 +0800

    Update deleteproduct.php

--- admin/deleteproduct.php
+++ admin/deleteproduct.php
@@ -3,9 +3,10 @@
    require_once "../database/products_class.php";
     
     if (isset($_POST['action']) && $_POST['action'] == 'delete') {
-        
+        print_r($_POST['action']);
         $product = new Products();
         $product->productId = $_POST['product_id'];
+
             if($product->delete()){
                 header('location: products.php');
             }
