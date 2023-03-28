2ca6a5b166b36c1212e0794ca1fd3d83b3333173
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-03-29 04:15:35 +0800

    Update addorder.php

--- order/addorder.php
+++ order/addorder.php
@@ -1,7 +1,7 @@
 <?php 
 require_once '../database/database.php';
-require_once "../database/user.class.php";
-
+require_once "../database/service_parameter.class.php";
+require_once "../database/service_order.class.php";
 // Check if the fosaverm has been submitted
 if (isset($_POST['action']) && $_POST['action'] == 'Continue') {
    
