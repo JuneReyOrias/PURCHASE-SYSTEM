aba9bf8b3bd8c3556c2186ab3bb04d580f08539a
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-04-11 02:34:22 +0800

    Update mutipletable.class.php

--- database/mutipletable.class.php
+++ database/mutipletable.class.php
@@ -3,10 +3,19 @@ require_once 'database.php';
 
 class multi{
     public $serviceCatId;
+    public $serviceCategory;
+    public $description;
+ //service parameter
     public $size;
     public $unitPrice;
     public $printType;
-    public $colorType;
+    // service order
+    public $materialId;
+    public $qty;
+    public $serviceId;
+    public $totalPrice;
+    public $filename;
+    
     protected $db;
 
     function __construct() {
@@ -16,21 +25,37 @@ class multi{
     function createProgram() {
         try {
     
-            // Get the ID of the semester with the given semester name
-            $collegeSql = "SELECT id FROM colleges WHERE id = :college_id";
-            $collegeStmt = $this->db->connect()->prepare($collegeSql);
-            $collegeStmt->bindParam(':college_id', $this->collegeID);
-            $collegeStmt->execute();
-            $collegeID = $collegeStmt->fetchColumn();
-    
-    
-            // Insert the new row into the fee_schedule table with the retrieved ID values
-            $insertSql = "INSERT INTO programs (program_name, college_id)
-            VALUES (:program, :college_id)";
-            $insertStmt = $this->db->connect()->prepare($insertSql);
-            $insertStmt->bindParam(':program', $this->programName);
-            $insertStmt->bindParam(':college_id', $collegeID);
-            $insertStmt->execute();
+ 
+
+// Insert data into table1
+$table1_sql = "INSERT INTO service_category (service_category, description) VALUES (:service_order, :description)";
+$table1_stmt= $this->db->connect()->prepare($table1_sql);
+$table1_stmt->bindParam(':service_category', $serviceCategory);
+$table1_stmt->bindParam(':description', $description);
+
+$table1_stmt->execute();
+
+// Insert data into table2
+$table2_sql = "INSERT INTO serivce_parameter (size, unit_price, print_type, color_type) VALUES (:order_id, :product_name, :quantity)";
+$table2_stmt = $this->db->connect()->prepare($table2_sql);
+$table2_stmt->bindParam(':size', $size);
+$table2_stmt->bindParam(':unit_price', $unitPrice);
+$table2_stmt->bindParam(':print_type', $printType);
+$table2_stmt->bindParam(':color_type', $colorType);
+$table2_stmt->execute();
+
+// Insert data into table3
+$table3_sql = "INSERT INTO serivce_order(material_id, qty, serviceCat_id, total_price, filename) VALUES (:material_id, :qty, :serviceCat_id, :filename)";
+$table3_stmt =$this->db->connect()->prepare($table3_sql);
+$table3_stmt->bindParam(':material_id', $materialId);
+$table3_stmt->bindParam(':qty', $qty);
+$table3_stmt->bindParam(':serviceCat_id', $serviceCatId);
+$table3_stmt->bindParam(':total_price', $totalPrice);
+$table3_stmt->bindParam(':filename', $filename);
+$table3_stmt->execute();
+
+echo "Data inserted successfully\n";
+
 
     
             return true;
