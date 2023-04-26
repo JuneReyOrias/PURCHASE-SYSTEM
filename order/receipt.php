498cd8b2322c9a774e71af4b49361ad74a76c898
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-04-27 01:18:45 +0800

    Update receipt.php

--- order/receipt.php
+++ order/receipt.php
@@ -392,9 +392,9 @@
         <ul class="list-inline">
   <li class="Prepared by:"style="margin-left:13rem">Prepared by:</li>
 
-  <li ><input type="text" class="text-center" name=total value="Joe 0. Julian" ></li></li>
+  <li ><input type="text" class="text-center" name=total value="Joe 0. Julian" disabled ></li></li>
 
-  <li class="Total:" style="margin-left:28rem;">Total:</li>
+  <li class="Total:" style="margin-left:24rem;">Total:</li>
 
   <li ><input type="decimal" class="text-center" name=total value="480.0" style="width:9rem; heigth:7rem;"disabled></li></li>
 </ul>
