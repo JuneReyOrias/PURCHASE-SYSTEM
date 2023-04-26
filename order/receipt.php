c1e64fd218cabaebcbed448c7073857dbc9af7c3
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2023-04-26 22:15:01 +0800

    Update receipt.php

--- order/receipt.php
+++ order/receipt.php
@@ -388,10 +388,10 @@
             </tr>
         </tbody>
         </table>
-        <button onclick="window.print()">Print</button>
+       
         <input type="hidden" name="action" value="save">
     <button type="submit" class="btn btn-primary float-end" style="width:20rem; margin-right: 70rem;" >TrackOrder</button>
-    <button type="submit" onclick="window.print()" class="btn btn-primary float-end" style="width:20rem; margin-right: 1rem; " >Print</button>             </form> 
+    <button type="submit" onclick="window.print(form)" class="btn btn-primary float-end" style="width:20rem; margin-right: 1rem; " >Print</button>             </form> 
      
 
 
